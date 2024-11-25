9c1a1a45126152d1592c76e393571a615fcfdde4
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:16:26 +0000

    Revert "[Commit] Iteration 254 #errors = 225 <= 223."
    This reverts commit f7765ab5265b5774994d7f96da50ba72e9d775c6.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -64,7 +63,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        System.Collections.Generic.ICollection<ServiceDescriptor> temp = services;
+        var temp = services as ICollection<ServiceDescriptor>;
     }
 
     /// <summary>
