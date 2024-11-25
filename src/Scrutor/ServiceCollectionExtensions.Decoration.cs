b33722f9223d534956b97905fa1c3a088b2423b0
NoReply Amazon <no-reply@amazon.com>
2024-11-25 16:15:07 +0000

    Revert "[Commit] Iteration 77 #errors = 218 <= 218."
    This reverts commit 1c90a7d8be204b322046b62b14a2cd9ee613a205.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,6 +1,5 @@
-using Scrutor;
+﻿using Scrutor;
 using System;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -23,9 +22,6 @@ public static partial class ServiceCollectionExtensions
 
         return services.Decorate(typeof(TService), typeof(TDecorator));
     }
-
-    // Helper method to ensure IEnumerable<T> is recognized
-    private static IEnumerable<T> AsEnumerable<T>(this IEnumerable<T> source) => source;
 
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
