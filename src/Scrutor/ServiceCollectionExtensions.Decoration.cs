1c5891f9f1f48e259df3b5fc57ab72ea2f2c65b9
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:19:49 +0000

    [Commit] Iteration 260 #errors = 223 <= 223.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][23,23] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -7,6 +7,17 @@ using Microsoft.Extensions.DependencyInjection;
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
+internal static class Preconditions
+{
+    public static void NotNull<T>(T value, string name) where T : class
+    {
+        if (value == null)
+        {
+            throw new ArgumentNullException(name);
+        }
+    }
+}
+
 [PublicAPI]
 public static partial class ServiceCollectionExtensions
 {
