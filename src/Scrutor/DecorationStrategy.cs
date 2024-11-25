3ecf28d4b7b2c6338a0fb2b88b949be72f2e2a85
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:13:48 +0000

    Revert "[Commit] Iteration 449 #errors = 215 <= 215."
    This reverts commit 453a5838d2ef6fbc2794dea0a5b7fdbbdbdfd562.

--- src/Scrutor/DecorationStrategy.cs
+++ src/Scrutor/DecorationStrategy.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Scrutor;
@@ -23,13 +22,13 @@ public abstract class DecorationStrategy
     internal static DecorationStrategy WithFactory(Type serviceType, Func<object, IServiceProvider, object> decoratorFactory) => 
         Create(serviceType, decoratorType: null, decoratorFactory);
     
-    protected static Func<System.IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
+    protected static Func<IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
     {
         var instanceToDecorate = serviceProvider.GetRequiredService(serviceType);
         return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, instanceToDecorate);
     };
 
-    protected static Func<System.IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, System.IServiceProvider, object> decoratorFactory) => serviceProvider =>
+    protected static Func<IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, IServiceProvider, object> decoratorFactory) => serviceProvider =>
     {
         var instanceToDecorate = serviceProvider.GetRequiredService(decorated);
         return decoratorFactory(instanceToDecorate, serviceProvider);
