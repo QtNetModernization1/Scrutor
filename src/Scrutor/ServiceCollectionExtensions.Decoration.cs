4df287fd4f59f93cdd2bf4a75c718dea4e404be4
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:21:09 +0000

    [Commit] Iteration 262 #errors = 223 <= 223.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][23,23] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -12,17 +12,41 @@ public static partial class ServiceCollectionExtensions
 {
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
-    /// using the specified type <typeparamref name="TDecorator"/>.
+/// using the specified type <typeparamref name="TDecorator"/>.
     /// </summary>
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
-    public static IServiceCollection Decorate<TService, TDecorator>(this IServiceCollection services)
+    public static System.Collections.Generic.IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this System.Collections.Generic.IEnumerable<ServiceDescriptor> services)
         where TDecorator : TService
     {
-        Preconditions.NotNull(services, nameof(services));
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
 
-        return services.Decorate(typeof(TService), typeof(TDecorator));
+        return DecorateInternal(services, typeof(TService), typeof(TDecorator));
+    }
+
+    private static System.Collections.Generic.IEnumerable<ServiceDescriptor> DecorateInternal(System.Collections.Generic.IEnumerable<ServiceDescriptor> services, Type serviceType, Type decoratorType)
+    {
+        foreach (var descriptor in services)
+        {
+            if (descriptor.ServiceType == serviceType)
+            {
+                yield return ServiceDescriptor.Describe(
+                    serviceType,
+                    sp => {
+                        var innerService = sp.GetRequiredService(serviceType);
+                        return (TService)ActivatorUtilities.CreateInstance(sp, decoratorType, innerService);
+                    },
+                    descriptor.Lifetime);
+            }
+            else
+            {
+                yield return descriptor;
+            }
+        }
     }
 
     /// <summary>
