b3af2d4437fb62f0e427b2f46503e43b89abf88b
NoReply Amazon <no-reply@amazon.com>
2024-11-25 21:26:06 +0000

    [Commit] Iteration 567 #errors = 220 <= 192.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0051][48,26] Inconsistent accessibility: parameter type 'RegistrationStrategy.IServiceCollection' is less accessible than method 'RegistrationStrategy.Apply(RegistrationStrategy.IServiceCollection, ServiceDescriptor)'``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -45,16 +45,14 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
+    public abstract void Apply(Microsoft.Extensions.DependencyInjection.IServiceCollection services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) => services.TryAdd(descriptor);
+        public override void Apply(Microsoft.Extensions.DependencyInjection.IServiceCollection services, ServiceDescriptor descriptor) => services.TryAdd(descriptor);
     }
 
-    private interface IServiceCollection : IList<ServiceDescriptor>
-    {
-    }
+    // Removed private IServiceCollection interface
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
@@ -68,7 +66,7 @@ public abstract class RegistrationStrategy
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services.HasRegistration(descriptor.ServiceType))
+            if (services.Any(s => s.ServiceType == descriptor.ServiceType))
             {
                 throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
             }
