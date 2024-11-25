0e6d338a0623b272776239603beb3d94cce3f277
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:34:53 +0000

    [Commit] Iteration 284 #errors = 215 <= 212.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][44,23] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,6 +1,7 @@
 using Scrutor;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,7 +18,7 @@ public static partial class ServiceCollectionExtensions
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
-    public static IEnumerable Decorate<TService, TDecorator>(this IEnumerable services)
+    public static IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this IEnumerable<ServiceDescriptor> services)
         where TDecorator : TService
     {
         if (services == null)
@@ -26,7 +27,7 @@ public static partial class ServiceCollectionExtensions
         return DecorateInternal(services, typeof(TService), typeof(TDecorator));
     }
 
-    private static IEnumerable DecorateInternal(IEnumerable services, Type serviceType, Type decoratorType)
+    private static IEnumerable<ServiceDescriptor> DecorateInternal(IEnumerable<ServiceDescriptor> services, Type serviceType, Type decoratorType)
     {
         // Implementation details...
         return services;
