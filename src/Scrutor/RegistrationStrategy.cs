a7eef46e06f3f21521e14739c20a3fbd7eef728a
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:20:55 +0000

    Revert "[Commit] Iteration 658 #errors = 207 <= 207."
    This reverts commit c6215ea50fe96ae833ff52b1048a81eb4fbdf12c.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -52,7 +52,7 @@ public abstract class RegistrationStrategy
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            ((IList<ServiceDescriptor>)services).Add(descriptor);
+            ((ICollection<ServiceDescriptor>)services).Add(descriptor);
         }
     }
 
