c3b1899d6c06fb256b15119b2e29aefa8ed5ae6b
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:38:56 +0000

    Revert "[Commit] Iteration 688 #errors = 208 <= 208."
    This reverts commit 3b1b243cdac4e92f9fe77d788fccffe7a7956d3a.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -5,10 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
 
-#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-using System.Collections.Generic;
-#endif
-
 namespace Scrutor;
 
 public abstract class RegistrationStrategy
