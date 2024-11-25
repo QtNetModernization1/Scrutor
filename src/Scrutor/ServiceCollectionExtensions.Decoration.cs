7b30d99945c77b04f0bdb18086365439f37dd5a7
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:59:25 +0000

    Revert "[Commit] Iteration 325 #errors = 226 <= 219."
    This reverts commit 291ceebc9ea429b538a83c65e5d2b7cc8b0f7a99.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -49,13 +49,13 @@ public static partial class ServiceCollectionExtensions
         return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
 
-    // Overload to handle IServiceCollection explicitly
-    public static bool TryDecorate<TService, TDecorator>(this IServiceCollection services)
+    // Overload to handle IList<ServiceDescriptor> explicitly
+    public static bool TryDecorate<TService, TDecorator>(this IList<ServiceDescriptor> services)
         where TDecorator : TService
     {
         Preconditions.NotNull(services, nameof(services));
 
-        return services.TryDecorate(typeof(TService), typeof(TDecorator));
+        return ((IServiceCollection)services).TryDecorate(typeof(TService), typeof(TDecorator));
     }
 
     // Helper method to ensure IServiceCollection is recognized
