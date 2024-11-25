8e0a54f52562b430a1675bab7c6cdec9e556de7c
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:26:07 +0000

    [Commit] Iteration 270 #errors = 223 <= 223.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][25,25] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,12 @@ public static partial class ServiceCollectionExtensions
         Preconditions.NotNull(services, nameof(services));
 
         return services.Decorate(typeof(TService), typeof(TDecorator));
+    }
+
+    // Helper method to ensure IList is recognized
+    private static void EnsureIListIsRecognized()
+    {
+        IList<object> list = new List<object>();
     }
 
     /// <summary>
