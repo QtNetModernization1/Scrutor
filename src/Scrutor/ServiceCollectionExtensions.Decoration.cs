7622d6b8b34a70c90c1beaf9435cd2551e323130
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:32:02 +0000

    [Commit] Iteration 380 #errors = 216 <= 215.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][31,20] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -7,6 +7,7 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
+using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -24,11 +25,11 @@ public static partial class ServiceCollectionExtensions
     }
 
     // Helper method to ensure IServiceCollection is recognized
-    private static void EnsureIServiceCollectionIsRecognized(IServiceCollection services)
+    private static void EnsureIServiceCollectionIsRecognized(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services as IEnumerable<ServiceDescriptor>;
+        var temp = services as System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
