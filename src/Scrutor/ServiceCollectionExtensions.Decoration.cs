e0ab0108adafdb14ac6e6cfb16dd1eb4ab16fa4d
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:28:30 +0000

    Revert "[Commit] Iteration 274 #errors = 224 <= 223."
    This reverts commit 8598d705a2c71eb122151967cee39e18670a6fa9.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
