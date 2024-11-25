3c326caba2c7c618e010e8f3c950d183f3cb2854
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:04:14 +0000

    [Commit] Iteration 434 #errors = 215 <= 215.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][77,25] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -28,13 +28,19 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services as IEnumerable<ServiceDescriptor>;
+        var temp = services as System.Collections.Generic.IEnumerable<ServiceDescriptor>;
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
     private static void EnsureIListIsRecognized<T>()
     {
         System.Collections.Generic.IList<T> list = new System.Collections.Generic.List<T>();
+    }
+
+    // Helper method to ensure System.Collections.Generic.ICollection<T> is recognized
+    private static void EnsureICollectionIsRecognized<T>()
+    {
+        System.Collections.Generic.ICollection<T> collection = new System.Collections.Generic.List<T>();
     }
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
