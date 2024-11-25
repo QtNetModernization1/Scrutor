994d7c576a75bcebb4a21fab560a93bd3d33cf19
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:51:43 +0000

    Revert "[Commit] Iteration 714 #errors = 208 <= 208."
    This reverts commit b29880506c58d7b0bf41b12ca71588285d819f10.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -7,7 +7,6 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
-using IServiceCollection = Microsoft.Extensions.DependencyInjection.IServiceCollection;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -16,7 +15,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static partial class ServiceCollectionExtensions
 {
     // Overload to handle IServiceCollection explicitly
-    public static bool TryDecorate<TService, TDecorator>(this ICollection<ServiceDescriptor> services)
+    public static bool TryDecorate<TService, TDecorator>(this System.Collections.Generic.ICollection<ServiceDescriptor> services)
         where TDecorator : TService
     {
         Preconditions.NotNull(services, nameof(services));
