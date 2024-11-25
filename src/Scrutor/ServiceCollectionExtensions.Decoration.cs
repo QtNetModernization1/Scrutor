f25646b4c238519a954885bb8059fa8ee35105b6
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:22:31 +0000

    Revert "[Commit] Iteration 364 #errors = 219 <= 219."
    This reverts commit d3cf7b42ade84167b3d28d1db21c2863fcdefc54.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static partial class ServiceCollectionExtensions
 {
     // Overload to handle IList<ServiceDescriptor> explicitly
-    public static bool TryDecorate<TService, TDecorator>(this System.Collections.Generic.IList<ServiceDescriptor> services)
+    public static bool TryDecorate<TService, TDecorator>(this IList<ServiceDescriptor> services)
         where TDecorator : TService
     {
         Preconditions.NotNull(services, nameof(services));
