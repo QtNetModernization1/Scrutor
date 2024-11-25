a7fc6c21664cd86e7c7da0a1ef48e459f963eed6
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:26:30 +0000

    [Commit] Iteration 470 #errors = 215 <= 215.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][91,23] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -2,7 +2,6 @@ using Scrutor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using GenericCollections = System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -88,9 +87,12 @@ public static partial class ServiceCollectionExtensions
     /// <paramref name="serviceType"/> or <paramref name="decoratorType"/> arguments are <c>null</c>.</exception>
     public static bool TryDecorate(this IServiceCollection services, Type serviceType, Type decoratorType)
     {
-        Preconditions.NotNull(services, nameof(services));
-        Preconditions.NotNull(serviceType, nameof(serviceType));
-        Preconditions.NotNull(decoratorType, nameof(decoratorType));
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+        if (serviceType == null)
+            throw new ArgumentNullException(nameof(serviceType));
+        if (decoratorType == null)
+            throw new ArgumentNullException(nameof(decoratorType));
 
         return services.TryDecorate(DecorationStrategy.WithType(serviceType, decoratorType));
     }
