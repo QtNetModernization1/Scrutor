937593231628fb8e56c22e63dc7d4bc430a3170b
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:58:26 +0000

    Revert "[Commit] Iteration 837 #errors = 208 <= 208."
    This reverts commit 946c51d664e46a5a2c5d603fabdecfbc46aa4d57.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -34,13 +34,13 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services.ToArray();
+        var temp = services.ToList();
     }
 
-    // Helper method to ensure System.Collections.Generic.IEnumerable<T> is recognized
-    private static void EnsureIEnumerableIsRecognized<T>()
+    // Helper method to ensure System.Collections.Generic.IList<T> is recognized
+    private static void EnsureIListIsRecognized<T>()
     {
-        System.Collections.Generic.IEnumerable<T> enumerable = new System.Collections.Generic.List<T>();
+        System.Collections.Generic.IList<T> list = new System.Collections.Generic.List<T>();
     }
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
