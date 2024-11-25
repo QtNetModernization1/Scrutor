9a45502b0d32c65ff96ab2ed999b49e3f29d1224
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:05:58 +0000

    Revert "[Commit] Iteration 335 #errors = 220 <= 219."
    This reverts commit 47b26408acbef2ad48fbd26f3c76e189a03de68b.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -46,7 +46,7 @@ public static partial class ServiceCollectionExtensions
     {
         Preconditions.NotNull(services, nameof(services));
 
-        return ((IEnumerable<ServiceDescriptor>)services).TryDecorate(typeof(TService), typeof(TDecorator));
+        return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
 
     // Overload to handle IList<ServiceDescriptor> explicitly
