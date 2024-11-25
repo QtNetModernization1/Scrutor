946c51d664e46a5a2c5d603fabdecfbc46aa4d57
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:58:26 +0000

    [Commit] Iteration 837 #errors = 208 <= 208.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][37,29] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -34,13 +34,13 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services.ToList();
+        var temp = services.ToArray();
     }
 
-    // Helper method to ensure System.Collections.Generic.IList<T> is recognized
-    private static void EnsureIListIsRecognized<T>()
+    // Helper method to ensure System.Collections.Generic.IEnumerable<T> is recognized
+    private static void EnsureIEnumerableIsRecognized<T>()
     {
-        System.Collections.Generic.IList<T> list = new System.Collections.Generic.List<T>();
+        System.Collections.Generic.IEnumerable<T> enumerable = new System.Collections.Generic.List<T>();
     }
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
