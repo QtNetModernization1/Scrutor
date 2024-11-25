1dc9879718dc194701ed761d5b72b2a049634859
NoReply Amazon <no-reply@amazon.com>
2024-11-25 21:23:09 +0000

    Revert "[Commit] Iteration 564 #errors = 217 <= 192."
    This reverts commit e2e811267a0984cc0a5c9b1e197bc064a5c0bda2.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -45,14 +45,16 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(Microsoft.Extensions.DependencyInjection.IServiceCollection services, ServiceDescriptor descriptor);
+    public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(Microsoft.Extensions.DependencyInjection.IServiceCollection services, ServiceDescriptor descriptor) => services.TryAdd(descriptor);
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) => services.TryAdd(descriptor);
     }
 
-    // Removed private IServiceCollection interface
+    private interface IServiceCollection : IList<ServiceDescriptor>
+    {
+    }
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
