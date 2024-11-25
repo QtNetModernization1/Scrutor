3c7f5e291c26adc5e6d0746c58ddce3bf8e4fd58
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:46:52 +0000

    Revert "[Commit] Iteration 305 #errors = 215 <= 215."
    This reverts commit fac7ba146625bec2fe2a5d950663a07efd417160.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -54,7 +54,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services as System.Collections.Generic.ICollection<ServiceDescriptor>;
+        var temp = services as ICollection<ServiceDescriptor>;
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
