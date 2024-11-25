dc52c727d542318c916539f22b71d44711b65955
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:10:48 +0000

    [Commit] Iteration 343 #errors = 220 <= 219.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][49,25] The type 'IEnumerable' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -6,6 +6,7 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
+using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -50,7 +51,7 @@ public static partial class ServiceCollectionExtensions
     }
 
     // Overload to handle IList<ServiceDescriptor> explicitly
-    public static bool TryDecorate<TService, TDecorator>(this IList<ServiceDescriptor> services)
+    public static bool TryDecorate<TService, TDecorator>(this System.Collections.Generic.IList<ServiceDescriptor> services)
         where TDecorator : TService
     {
         Preconditions.NotNull(services, nameof(services));
