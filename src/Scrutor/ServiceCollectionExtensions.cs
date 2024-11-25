3483a84335b1d1b10b4d4253533fcc3653e016ad
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:40:01 +0000

    [Commit] Iteration 803 #errors = 216 <= 216.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.cs][CS0012][11,25] The type 'IEnumerable' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.cs
+++ src/Scrutor/ServiceCollectionExtensions.cs
@@ -8,6 +8,6 @@ internal static class ServiceCollectionExtensions
 {
     public static bool HasRegistration(this IServiceCollection services, Type serviceType)
     {
-        return services.Any(x => x.ServiceType == serviceType);
+        return services.AsEnumerable().Any(x => x.ServiceType == serviceType);
     }
 }
