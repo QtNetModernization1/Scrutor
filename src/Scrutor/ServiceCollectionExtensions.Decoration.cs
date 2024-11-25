22328ee888ead8e480cb392dcd4414554a146e39
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:53:40 +0000

    Revert "[Commit] Iteration 418 #errors = 216 <= 215."
    This reverts commit 3ac41b07866a94f41d8cdf36390bfa5bb3a6eb89.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -7,7 +7,6 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
-using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -44,7 +43,7 @@ public static partial class ServiceCollectionExtensions
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
-    public static System.Collections.Generic.IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this System.Collections.Generic.IEnumerable<ServiceDescriptor> services)
+    public static IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this IEnumerable<ServiceDescriptor> services)
         where TDecorator : TService
     {
         if (services == null)
