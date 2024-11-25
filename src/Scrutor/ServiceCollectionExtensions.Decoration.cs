bc65fba7e5e74e15a62217b24469cf14a5c0934c
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:23:38 +0000

    Revert "[Commit] Iteration 465 #errors = 215 <= 215."
    This reverts commit 6a9cc345355f59d7583bba59885ff559c123e24f.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -34,7 +34,7 @@ public static partial class ServiceCollectionExtensions
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
     private static void EnsureIListIsRecognized<T>()
     {
-        GenericCollections.IList<T> list = new GenericCollections.List<T>();
+        System.Collections.Generic.IList<T> list = new System.Collections.Generic.List<T>();
     }
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
