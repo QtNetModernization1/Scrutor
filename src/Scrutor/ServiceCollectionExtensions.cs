406e4c2a19877e9243117b78b16468d0d4b9a29b
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:43:45 +0000

    Revert "[Commit] Iteration 809 #errors = 213 <= 216."
    This reverts commit b43ec1a9f0eafff35e045cd3bf01aabac9100df5.

--- src/Scrutor/ServiceCollectionExtensions.cs
+++ src/Scrutor/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +8,6 @@ internal static class ServiceCollectionExtensions
 {
     public static bool HasRegistration(this IServiceCollection services, Type serviceType)
     {
-        return ((IList<ServiceDescriptor>)services).Any(x => x.ServiceType == serviceType);
+        return services.Any(x => x.ServiceType == serviceType);
     }
 }
