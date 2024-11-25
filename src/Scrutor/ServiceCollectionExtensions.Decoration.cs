8c59a697bcf72dfa7453ade871e358e5c1aea5dd
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:27:50 +0000

    Revert "[Commit] Iteration 373 #errors = 215 <= 215."
    This reverts commit 0c52671e57ef41d687d5dc7374be3205604a6d0d.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -2,6 +2,7 @@ using Scrutor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using GenericCollections = System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
