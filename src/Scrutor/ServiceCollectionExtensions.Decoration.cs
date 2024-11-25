6c6da36d9d3f66c330c45d4f0e7d618c182de1b5
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:32:45 +0000

    Revert "[Commit] Iteration 480 #errors = 215 <= 215."
    This reverts commit a24285f44a1a9d1bb1b32238ed85b3193efaef5d.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -23,12 +23,6 @@ public static partial class ServiceCollectionExtensions
         return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
 
-    // Helper method to ensure IEnumerable<T> is recognized
-    private static void EnsureIEnumerableIsRecognized<T>()
-    {
-        System.Collections.Generic.IEnumerable<T> enumerable = new System.Collections.Generic.List<T>();
-    }
-
     // Helper method to ensure IServiceCollection is recognized
     private static void EnsureIServiceCollectionIsRecognized(IServiceCollection services)
     {
@@ -49,7 +43,7 @@ public static partial class ServiceCollectionExtensions
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
-    public static System.Collections.Generic.IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this System.Collections.Generic.IEnumerable<ServiceDescriptor> services)
+    public static IEnumerable<ServiceDescriptor> Decorate<TService, TDecorator>(this IEnumerable<ServiceDescriptor> services)
         where TDecorator : TService
     {
         if (services == null)
@@ -58,7 +52,7 @@ public static partial class ServiceCollectionExtensions
         return DecorateInternal(services, typeof(TService), typeof(TDecorator));
     }
 
-    private static System.Collections.Generic.IEnumerable<ServiceDescriptor> DecorateInternal(System.Collections.Generic.IEnumerable<ServiceDescriptor> services, Type serviceType, Type decoratorType)
+    private static IEnumerable<ServiceDescriptor> DecorateInternal(IEnumerable<ServiceDescriptor> services, Type serviceType, Type decoratorType)
     {
         // Implementation details...
         return services;
