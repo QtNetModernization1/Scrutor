0200bdf995b0979361d058af7439d441b1bd1687
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:32:01 +0000

    [Commit] Iteration 789 #errors = 216 <= 216.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.cs][CS0012][11,25] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.cs
+++ src/Scrutor/ServiceCollectionExtensions.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Scrutor;
 
 internal static class ServiceCollectionExtensions
 {
-    public static bool HasRegistration(this IServiceCollection services, Type serviceType)
+    public static bool HasRegistration(this Microsoft.Extensions.DependencyInjection.IServiceCollection services, Type serviceType)
     {
         return services.Any(x => x.ServiceType == serviceType);
     }
