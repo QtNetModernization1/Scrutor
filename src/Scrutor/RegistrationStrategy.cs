a47aac4bd7a8c104047cc5ee6363cacdcc322f5b
NoReply Amazon <no-reply@amazon.com>
2024-11-26 23:11:04 +0000

    Revert "[Commit] Iteration 224 #errors = 216 <= 216."
    This reverts commit a8d838f9188c2e66b088f2698332b57b21878e44.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -1,7 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
