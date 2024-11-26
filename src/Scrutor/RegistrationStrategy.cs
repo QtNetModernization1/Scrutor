9f30355666cef60b96933e6df15465c6e08da13d
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:52:37 +0000

    Revert "[Commit] Iteration 1045 #errors = 208 <= 208."
    This reverts commit 43bcc6c0681b43f25025d548a77fe168fb1ac1f5.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -9,7 +9,6 @@ namespace Scrutor;
 
 // Ensure IList<T> is recognized
 using IServiceDescriptorList = System.Collections.Generic.IList<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
-using System.Collections.Generic;
 
 public abstract class RegistrationStrategy
 {
