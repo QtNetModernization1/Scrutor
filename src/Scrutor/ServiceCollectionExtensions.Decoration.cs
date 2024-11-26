a3824886b36a196fe0f8a8e0c72528f92838f398
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:22:13 +0000

    [Commit] Iteration 879 #errors = 203 <= 202.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][37,21] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -34,7 +35,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = ((System.Collections.Generic.IEnumerable<ServiceDescriptor>)services).ToList();
+        var temp = ((IEnumerable<ServiceDescriptor>)services).ToList();
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
