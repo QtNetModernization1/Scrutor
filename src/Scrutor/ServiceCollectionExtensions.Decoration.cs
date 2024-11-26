0bf6363c3f79733feb4ef2d5802334972df84bdf
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:38:37 +0000

    Revert "[Commit] Iteration 908 #errors = 207 <= 206."
    This reverts commit 43020eda0e0f085799d99ac7a7d4c006de93816e.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
 using System.Collections.ObjectModel;
-using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -35,7 +34,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        System.Collections.Generic.IList<ServiceDescriptor> temp = services.ToList();
+        var temp = services.ToList();
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
