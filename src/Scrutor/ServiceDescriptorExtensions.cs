64ca2b04da6ea55c0618efa70b38ac54324f157c
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:53:29 +0000

    [Commit] Iteration 936 #errors = 206 <= 206.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceDescriptorExtensions.cs][CS0012][15,11] The type 'Func<,>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -5,14 +5,14 @@ namespace Scrutor;
 
 internal static class ServiceDescriptorExtensions
 {
-    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) =>
+    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, System.Func<IServiceProvider, object> implementationFactory) =>
         new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
     public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
         { ImplementationType: Type implementationType } when implementationType != null =>
             new ServiceDescriptor(serviceType, implementationType, descriptor.Lifetime),
-        { ImplementationFactory: Func<IServiceProvider, object> factory } when factory != null =>
+        { ImplementationFactory: System.Func<IServiceProvider, object> factory } when factory != null =>
             new ServiceDescriptor(serviceType, sp => factory(sp), descriptor.Lifetime),
         { ImplementationInstance: object instance } when instance != null =>
             new ServiceDescriptor(serviceType, instance),
