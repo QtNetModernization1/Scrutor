29e23c2eb5c0ff477d9e9c4d33a0b68e88b9701c
NoReply Amazon <no-reply@amazon.com>
2024-11-26 22:35:38 +0000

    Revert "[Commit] Iteration 174 #errors = 217 <= 216."
    This reverts commit 65937a5905fd6dfbdb494f097655edc25bcdf3e6.

--- src/Scrutor/ServiceDescriptorAttribute.cs
+++ src/Scrutor/ServiceDescriptorAttribute.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 
-// Explicitly include System namespace to ensure Enum is recognized
-using System;
-
 namespace Scrutor;
 
 [PublicAPI]
