0075578083867f2fb783949c7999c49f718e6e17
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:25:03 +0000

    [Commit] Iteration 884 #errors = 202 <= 202.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][37,21] The type 'IEnumerable' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -2,7 +2,6 @@ using Scrutor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using GenericCollections = System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
