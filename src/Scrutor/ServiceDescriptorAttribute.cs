3e37983c73b8fb98c9d830fa1a81b65bf2dabb1d
NoReply Amazon <no-reply@amazon.com>
2024-11-26 20:54:32 +0000

    Revert "[Commit] Iteration 23 #errors = 221 <= 215."
    This reverts commit 4ca0391b6407c90a34d2b67d554983b3934a9915.

--- src/Scrutor/ServiceDescriptorAttribute.cs
+++ src/Scrutor/ServiceDescriptorAttribute.cs
@@ -1,8 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
-using System.Runtime.InteropServices;
 
 namespace Scrutor;
 
@@ -22,15 +21,7 @@ public class ServiceDescriptorAttribute : Attribute
 
     public Type? ServiceType { get; }
 
-public ServiceLifetime Lifetime { get; }
-
-[Guid("cc7b13ff-cd2d-dd51-0000-000000000001")]
-public enum ServiceLifetimeEnum
-{
-    Singleton = ServiceLifetime.Singleton,
-    Scoped = ServiceLifetime.Scoped,
-    Transient = ServiceLifetime.Transient
-}
+    public ServiceLifetime Lifetime { get; }
 
     public IEnumerable<Type> GetServiceTypes(Type fallbackType)
     {
