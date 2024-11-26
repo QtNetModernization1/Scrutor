c84129be8134058f98138ee366e0c05412da0b42
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:43:04 +0000

    Revert "[Commit] Iteration 916 #errors = 206 <= 206."
    This reverts commit 0748a3a15d0e5a8e8491d22add2d13a06de739ec.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -34,7 +34,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> temp = services.ToList();
+        var temp = services.ToList();
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
