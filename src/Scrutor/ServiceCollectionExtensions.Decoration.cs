c7d59b96adbc4d3e3664d03d70ff43dd5d2c5722
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:27:18 +0000

    [Commit] Iteration 888 #errors = 206 <= 206.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][37,29] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -34,7 +34,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        var temp = services.ToList();
+        var temp = services.ToList<ServiceDescriptor>();
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
