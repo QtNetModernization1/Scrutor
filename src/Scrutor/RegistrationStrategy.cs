c9fb371be87673e47634b341d2549e09093585ed
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:14:02 +0000

    Revert "[Commit] Iteration 974 #errors = 206 <= 208."
    This reverts commit c1cab00e032cd6e36380bfe2733db9c2e07c1655.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -68,7 +68,7 @@ public abstract class RegistrationStrategy
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (System.Linq.Enumerable.Any<ServiceDescriptor>(services, s => s.ServiceType == descriptor.ServiceType))
+            if (services.Any(s => s.ServiceType == descriptor.ServiceType))
             {
                 throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
             }
