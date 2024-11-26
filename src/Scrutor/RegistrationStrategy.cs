473658f2e91cfd5a47e1bfaabc0a3bf531048493
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:41:32 +0000

    Revert "[Commit] Iteration 1023 #errors = 207 <= 207."
    This reverts commit 6db06b33ac004eb88ad72c7b9bf23df0bca57316.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -73,7 +73,7 @@ public abstract class RegistrationStrategy
                 throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
             }
 
-            ((ICollection<ServiceDescriptor>)services).Add(descriptor);
+            ((IList<ServiceDescriptor>)services).Add(descriptor);
         }
     }
 
