a11f72523b9764eeccca492327ba508fe4233a71
NoReply Amazon <no-reply@amazon.com>
2024-11-26 02:45:44 +0000

    [Commit] Iteration 1139 #errors = 193 <= 197.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][108,48] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -1,14 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
 
 namespace Scrutor;
-
-using IServiceDescriptorList = System.Collections.Generic.IList<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
-using ICollectionInterface = System.Collections.Generic.ICollection<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
 
 public abstract class RegistrationStrategy
 {
@@ -22,12 +17,12 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-public abstract void Apply(ICollectionInterface services, Microsoft.Extensions.DependencyInjection.ServiceDescriptor descriptor);
+    public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
 
-protected static void AddServiceDescriptor(ICollectionInterface services, Microsoft.Extensions.DependencyInjection.ServiceDescriptor descriptor)
-{
-    ((System.Collections.Generic.ICollection<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>)services).Add(descriptor);
-}
+    protected static void AddServiceDescriptor(IServiceCollection services, ServiceDescriptor descriptor)
+    {
+        services.Add(descriptor);
+    }
 
     /// <summary>
     /// Appends a new registration for existing services.
@@ -58,7 +53,7 @@ protected static void AddServiceDescriptor(ICollectionInterface services, Micros
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(ICollectionInterface services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
             AddServiceDescriptor(services, descriptor);
         }
@@ -66,7 +61,7 @@ protected static void AddServiceDescriptor(ICollectionInterface services, Micros
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(ICollectionInterface services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
             AddServiceDescriptor(services, descriptor);
         }
@@ -74,9 +69,9 @@ protected static void AddServiceDescriptor(ICollectionInterface services, Micros
 
     private sealed class ThrowRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(ICollectionInterface services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (((IEnumerable<ServiceDescriptor>)services).Any(s => s.ServiceType == descriptor.ServiceType))
+            if (services.Any(s => s.ServiceType == descriptor.ServiceType))
             {
                 throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
             }
@@ -94,7 +89,7 @@ protected static void AddServiceDescriptor(ICollectionInterface services, Micros
 
         private ReplacementBehavior Behavior { get; }
 
-        public override void Apply(ICollectionInterface services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
             var behavior = Behavior;
 
@@ -105,12 +100,12 @@ protected static void AddServiceDescriptor(ICollectionInterface services, Micros
 
             if (behavior.HasFlag(ReplacementBehavior.ServiceType))
             {
-                ((IServiceCollection)services).RemoveAll(s => s.ServiceType == descriptor.ServiceType);
+                services.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
             }
 
             if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
             {
-                ((IServiceCollection)services).RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
+                services.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
             }
 
             AddServiceDescriptor(services, descriptor);
