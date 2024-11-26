7430f778520bcdd3660463ae6a2f75ae0c5da3f1
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:03:16 +0000

    Revert "[Commit] Iteration 845 #errors = 204 <= 204."
    This reverts commit aa19ad3ca3daa701a926a7f7b38c0bbe5b771909.

--- src/Scrutor/DecorationStrategy.cs
+++ src/Scrutor/DecorationStrategy.cs
@@ -22,17 +22,17 @@ public abstract class DecorationStrategy
     internal static DecorationStrategy WithFactory(Type serviceType, Func<object, IServiceProvider, object> decoratorFactory) => 
         Create(serviceType, decoratorType: null, decoratorFactory);
     
-protected static Func<System.IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
-{
-    var instanceToDecorate = serviceProvider.GetRequiredService(serviceType);
-    return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, instanceToDecorate);
-};
+    protected static Func<IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
+    {
+        var instanceToDecorate = serviceProvider.GetRequiredService(serviceType);
+        return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, instanceToDecorate);
+    };
 
-protected static Func<System.IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, System.IServiceProvider, object> decoratorFactory) => serviceProvider =>
-{
-    var instanceToDecorate = serviceProvider.GetRequiredService(decorated);
-    return decoratorFactory(instanceToDecorate, serviceProvider);
-};
+    protected static Func<IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, IServiceProvider, object> decoratorFactory) => serviceProvider =>
+    {
+        var instanceToDecorate = serviceProvider.GetRequiredService(decorated);
+        return decoratorFactory(instanceToDecorate, serviceProvider);
+    };
 
     private static DecorationStrategy Create(Type serviceType, Type? decoratorType, Func<object, IServiceProvider, object>? decoratorFactory)
     {
