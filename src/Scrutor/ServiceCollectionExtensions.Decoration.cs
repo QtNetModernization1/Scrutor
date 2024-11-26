8c969f7f715abd7ee95b63cd98864e01a8ece9a8
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:34:43 +0000

    Revert "[Commit] Iteration 901 #errors = 206 <= 206."
    This reverts commit 69b54367ef9aec0cbaa20f38c3fbe30fb838c5b9.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -15,11 +15,6 @@ namespace Microsoft.Extensions.DependencyInjection;
 [PublicAPI]
 public static partial class ServiceCollectionExtensions
 {
-    // Ensure System.Collections.Generic.IEnumerable<T> is recognized
-    private static void EnsureGenericIEnumerableIsRecognized<T>()
-    {
-        System.Collections.Generic.IEnumerable<T> _ = new System.Collections.Generic.List<T>();
-    }
     // Overload to handle IServiceCollection explicitly
     public static bool TryDecorate<TService, TDecorator>(this IServiceCollection services)
         where TDecorator : TService
@@ -39,7 +34,7 @@ public static partial class ServiceCollectionExtensions
     {
         // This method is just to force the compiler to recognize IServiceCollection
         // It will never be called
-        System.Collections.Generic.IEnumerable<ServiceDescriptor> temp = services.ToList();
+        var temp = services.ToList();
     }
 
     // Helper method to ensure System.Collections.Generic.IList<T> is recognized
