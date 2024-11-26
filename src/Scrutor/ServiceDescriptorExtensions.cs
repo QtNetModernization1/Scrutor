96a7173c3410c1faa06e3da119b60a857cc60793
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:07:06 +0000

    Revert "[Commit] Iteration 961 #errors = 206 <= 206."
    This reverts commit 85397d044aae3a9e04332a48d4ea4ba731acec81.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -5,16 +5,16 @@ namespace Scrutor;
 
 internal static class ServiceDescriptorExtensions
 {
-    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, System.Object> implementationFactory) =>
+    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) =>
         new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
     public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
         { ImplementationType: Type implementationType } when implementationType != null =>
             new ServiceDescriptor(serviceType, implementationType, descriptor.Lifetime),
-        { ImplementationFactory: Func<IServiceProvider, System.Object> factory } when factory != null =>
+        { ImplementationFactory: Func<IServiceProvider, object> factory } when factory != null =>
             new ServiceDescriptor(serviceType, sp => factory(sp), descriptor.Lifetime),
-        { ImplementationInstance: System.Object instance } when instance != null =>
+        { ImplementationInstance: object instance } when instance != null =>
             new ServiceDescriptor(serviceType, instance),
         _ => throw new ArgumentException($"No implementation factory or instance or type found for {descriptor.ServiceType}.", nameof(descriptor))
     };
