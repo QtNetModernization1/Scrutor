1a9b5f066d1cbe449e8ec8d0e5b7787e3f5508d9
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:03:54 +0000

    Revert "[Commit] Iteration 547 #errors = 219 <= 219."
    This reverts commit bc046f8afae719de04ebe758228dd8b7312cc67a.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -5,14 +5,14 @@ namespace Scrutor;
 
 internal static class ServiceDescriptorExtensions
 {
-    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<System.IServiceProvider, object> implementationFactory) =>
+    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) => 
         new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
-    public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, System.Type serviceType) => descriptor switch
+    public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
         { ImplementationType: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationType, descriptor.Lifetime),
         { ImplementationFactory: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationFactory, descriptor.Lifetime),
         { ImplementationInstance: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationInstance),
-        _ => throw new System.ArgumentException($"No implementation factory or instance or type found for {descriptor.ServiceType}.", nameof(descriptor))
+        _ => throw new ArgumentException($"No implementation factory or instance or type found for {descriptor.ServiceType}.", nameof(descriptor))
     };
 }
