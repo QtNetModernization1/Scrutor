3fb2974340ad9172b94ff8041b1a90b057bebaf3
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:28:12 +0000

    Revert "[Commit] Iteration 583 #errors = 220 <= 220."
    This reverts commit a86f0cd6e3066edf1491d7052e9218eea4e6a720.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,11 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Collections;
 
 namespace Scrutor;
 
@@ -41,11 +41,12 @@ internal sealed class LifetimeSelector : ILifetimeSelector, ISelector
         return WithLifetime(Microsoft.Extensions.DependencyInjection.ServiceLifetime.Transient);
     }
 
-public IImplementationTypeSelector WithLifetime(ServiceLifetime lifetime)
+public IImplementationTypeSelector WithLifetime(Microsoft.Extensions.DependencyInjection.ServiceLifetime lifetime)
 {
-    if (!Enum.IsDefined(typeof(ServiceLifetime), lifetime))
+    var values = System.Enum.GetValues(typeof(Microsoft.Extensions.DependencyInjection.ServiceLifetime));
+    if (!((IList)values).Contains(lifetime))
     {
-        throw new ArgumentException($"The value of argument '{nameof(lifetime)}' ({lifetime}) is invalid for Enum type '{nameof(ServiceLifetime)}'.", nameof(lifetime));
+        throw new ArgumentException($"The value of argument '{nameof(lifetime)}' ({lifetime}) is invalid for Enum type '{nameof(Microsoft.Extensions.DependencyInjection.ServiceLifetime)}'.", nameof(lifetime));
     }
 
     Inner.PropagateLifetime(lifetime);
