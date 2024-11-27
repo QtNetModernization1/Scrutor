2dc3ecbc6b7faa3a54f0e6b7a6eae93edd08507d
NoReply Amazon <no-reply@amazon.com>
2024-11-27 02:03:43 +0000

    Revert "[Commit] Iteration 638 #errors = 220 <= 220."
    This reverts commit d0e55b719adc8eec4586c7285268b9e411c97efa.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -43,7 +43,8 @@ internal sealed class LifetimeSelector : ILifetimeSelector, ISelector
 
 public IImplementationTypeSelector WithLifetime(Microsoft.Extensions.DependencyInjection.ServiceLifetime lifetime)
 {
-    if (!Enum.IsDefined(typeof(Microsoft.Extensions.DependencyInjection.ServiceLifetime), lifetime))
+    var values = System.Enum.GetValues(typeof(Microsoft.Extensions.DependencyInjection.ServiceLifetime));
+    if (!((IList)values).Contains(lifetime))
     {
         throw new ArgumentException($"The value of argument '{nameof(lifetime)}' ({lifetime}) is invalid for Enum type '{nameof(Microsoft.Extensions.DependencyInjection.ServiceLifetime)}'.", nameof(lifetime));
     }
