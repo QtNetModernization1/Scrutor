428401e3eb7d9981a5d1d0db8a1573174fdcdb2c
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:18:30 +0000

    [Commit] Iteration 569 #errors = 223 <= 220.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/LifetimeSelector.cs][CS0012][25,53] The type 'Enum' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
 using System.Runtime.InteropServices;
+using System.Linq;
 
 namespace Scrutor;
 
@@ -41,7 +42,7 @@ internal sealed class LifetimeSelector : ILifetimeSelector, ISelector
 
 public IImplementationTypeSelector WithLifetime(Microsoft.Extensions.DependencyInjection.ServiceLifetime lifetime)
 {
-    if (!Enum.IsDefined(typeof(Microsoft.Extensions.DependencyInjection.ServiceLifetime), lifetime))
+    if (!Enum.GetValues(typeof(Microsoft.Extensions.DependencyInjection.ServiceLifetime)).Cast<Microsoft.Extensions.DependencyInjection.ServiceLifetime>().Contains(lifetime))
     {
         throw new ArgumentException($"The value of argument '{nameof(lifetime)}' ({lifetime}) is invalid for Enum type '{nameof(Microsoft.Extensions.DependencyInjection.ServiceLifetime)}'.", nameof(lifetime));
     }
