02d4c618deed0deeac4549bfd48e6e0266095ac1
NoReply Amazon <no-reply@amazon.com>
2024-11-27 00:42:36 +0000

    Revert "[Commit] Iteration 515 #errors = 219 <= 219."
    This reverts commit fa4f2b8ae020cd43b63f58be27465bc8a9fca87f.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Scrutor;
 
-using System;
-
 internal static class ServiceDescriptorExtensions
 {
     public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) => 
