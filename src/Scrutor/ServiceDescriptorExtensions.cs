245c06d975f3645a1e66204acdba21d93f95ce80
NoReply Amazon <no-reply@amazon.com>
2024-12-09 21:55:52 +0000

    [Commit] Iteration 550 #errors = 284 <= 285.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceDescriptorExtensions.cs][CS0012][13,11] The type 'Type' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -5,9 +5,10 @@ namespace Scrutor;
 
 internal static class ServiceDescriptorExtensions
 {
-    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) => 
+    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) =>
         new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
+#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER || NET6_0_OR_GREATER
     public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
         { ImplementationType: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationType, descriptor.Lifetime),
@@ -15,4 +16,22 @@ internal static class ServiceDescriptorExtensions
         { ImplementationInstance: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationInstance),
         _ => throw new ArgumentException($"No implementation factory or instance or type found for {descriptor.ServiceType}.", nameof(descriptor))
     };
+#else
+    public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType)
+    {
+        if (descriptor.ImplementationType != null)
+        {
+            return new ServiceDescriptor(serviceType, descriptor.ImplementationType, descriptor.Lifetime);
+        }
+        if (descriptor.ImplementationFactory != null)
+        {
+            return new ServiceDescriptor(serviceType, descriptor.ImplementationFactory, descriptor.Lifetime);
+        }
+        if (descriptor.ImplementationInstance != null)
+        {
+            return new ServiceDescriptor(serviceType, descriptor.ImplementationInstance);
+        }
+        throw new ArgumentException($"No implementation factory or instance or type found for {descriptor.ServiceType}.", nameof(descriptor));
+    }
+#endif
 }
