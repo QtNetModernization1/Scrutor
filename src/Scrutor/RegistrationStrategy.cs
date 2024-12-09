a6ac77a692791fad6f5b59064ccae46997fcf131
NoReply Amazon <no-reply@amazon.com>
2024-12-09 22:44:54 +0000

    Revert "[Commit] Iteration 631 #errors = 287 <= 287."
    This reverts commit 615e43259b77b0d4a31fb55b53c4ed70c5a2d819.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
-using System.Reflection;
 
 #if NETSTANDARD2_0 || NET461
 // Removed duplicate using directive
