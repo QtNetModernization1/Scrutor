2bc1c663894dd713554b86e64babdf428aa364f4
NoReply Amazon <no-reply@amazon.com>
2024-12-09 17:27:19 +0000

    Revert "[Commit] Iteration 104 #errors = 303 <= 302."
    This reverts commit 486c8e93066b4d89cca6a9ab57ffe1b7318c88fa.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,6 +1,5 @@
-using Scrutor;
+﻿using Scrutor;
 using System;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -22,12 +21,6 @@ public static partial class ServiceCollectionExtensions
         Preconditions.NotNull(services, nameof(services));
 
         return services.Decorate(typeof(TService), typeof(TDecorator));
-    }
-
-    // Add this extension method to ensure IServiceCollection is recognized
-    public static IServiceCollection AsServiceCollection(this IEnumerable<ServiceDescriptor> services)
-    {
-        return services as IServiceCollection ?? new ServiceCollection(services);
     }
 
     /// <summary>
