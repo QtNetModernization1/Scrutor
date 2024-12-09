7348b73d513160d03d03b249ab261405b7ad9bb1
NoReply Amazon <no-reply@amazon.com>
2024-12-09 18:33:42 +0000

    [Commit] Iteration 205 #errors = 292 <= 294.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][56,107] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -93,7 +93,7 @@ public abstract class RegistrationStrategy
                 {
                     if (services[i].ServiceType == descriptor.ServiceType)
                     {
-                        services.RemoveAt(i);
+                        ((System.Collections.IList)services).RemoveAt(i);
                     }
                 }
             }
@@ -104,7 +104,7 @@ public abstract class RegistrationStrategy
                 {
                     if (services[i].ImplementationType == descriptor.ImplementationType)
                     {
-                        services.RemoveAt(i);
+                        ((System.Collections.IList)services).RemoveAt(i);
                     }
                 }
             }
