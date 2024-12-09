486929c13963a1c64f26ce1c8074f9ab5a8878d7
NoReply Amazon <no-reply@amazon.com>
2024-12-09 17:56:58 +0000

    [Commit] Iteration 146 #errors = 287 <= 288.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/LifetimeSelector.cs][CS0453][43,23] The type 'ServiceLifetime' must be a non-nullable value type in order to use it as parameter 'TEnum' in the generic type or method 'Preconditions.IsDefined<TEnum>(TEnum, string)'``.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,8 +40,6 @@ internal sealed class LifetimeSelector : ILifetimeSelector, ISelector
 
     public IImplementationTypeSelector WithLifetime(ServiceLifetime lifetime)
     {
-        Preconditions.IsDefined(lifetime, nameof(lifetime));
-
         Inner.PropagateLifetime(lifetime);
 
         return this;
