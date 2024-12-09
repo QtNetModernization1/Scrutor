615e43259b77b0d4a31fb55b53c4ed70c5a2d819
NoReply Amazon <no-reply@amazon.com>
2024-12-09 22:44:54 +0000

    [Commit] Iteration 631 #errors = 287 <= 287.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][75,26] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
+using System.Reflection;
 
 #if NETSTANDARD2_0 || NET461
 // Removed duplicate using directive
