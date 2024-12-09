2e179a67a8174ac080547f26f49dc1b1e4b50ca6
NoReply Amazon <no-reply@amazon.com>
2024-12-09 18:14:14 +0000

    Revert "[Commit] Iteration 175 #errors = 310 <= 308."
    This reverts commit b1f0ac8c346dec1bbbda8db29d9d117160beca0c.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -84,14 +84,8 @@ public abstract class RegistrationStrategy
 
         private ReplacementBehavior Behavior { get; }
 
-        public override void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            var serviceCollection = services as IServiceCollection;
-            if (serviceCollection == null)
-            {
-                throw new System.ArgumentException("Services must be of type IServiceCollection", nameof(services));
-            }
-
             var behavior = Behavior;
 
             if (behavior == ReplacementBehavior.Default)
@@ -101,27 +95,27 @@ public abstract class RegistrationStrategy
 
             if (behavior.HasFlag(ReplacementBehavior.ServiceType))
             {
-                for (var i = serviceCollection.Count - 1; i >= 0; i--)
+                for (var i = services.Count - 1; i >= 0; i--)
                 {
-                    if (serviceCollection[i].ServiceType == descriptor.ServiceType)
+                    if (services[i].ServiceType == descriptor.ServiceType)
                     {
-                        serviceCollection.RemoveAt(i);
+                        services.RemoveAt(i);
                     }
                 }
             }
 
             if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
             {
-                for (var i = serviceCollection.Count - 1; i >= 0; i--)
+                for (var i = services.Count - 1; i >= 0; i--)
                 {
-                    if (serviceCollection[i].ImplementationType == descriptor.ImplementationType)
+                    if (services[i].ImplementationType == descriptor.ImplementationType)
                     {
-                        serviceCollection.RemoveAt(i);
+                        services.RemoveAt(i);
                     }
                 }
             }
 
-            serviceCollection.Add(descriptor);
+            services.Add(descriptor);
         }
     }
 }
