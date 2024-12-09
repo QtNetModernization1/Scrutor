57cc43e84d01da5220e6a0c4d0861aa67c04e8a3
NoReply Amazon <no-reply@amazon.com>
2024-12-09 19:59:23 +0000

    [Commit] Iteration 354 #errors = 285 <= 285.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/DecorationStrategy.cs][CS0012][28,16] The type 'IServiceProvider' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/DecorationStrategy.cs
+++ src/Scrutor/DecorationStrategy.cs
@@ -9,32 +9,32 @@ public abstract class DecorationStrategy
     {
         ServiceType = serviceType;
     }
-    
+
     public Type ServiceType { get; }
-    
+
     public abstract bool CanDecorate(Type serviceType);
-    
-    public abstract Func<IServiceProvider, object> CreateDecorator(Type serviceType);
-    
-    internal static DecorationStrategy WithType(Type serviceType, Type decoratorType) => 
+
+    public abstract Func<System.IServiceProvider, object> CreateDecorator(Type serviceType);
+
+    internal static DecorationStrategy WithType(Type serviceType, Type decoratorType) =>
         Create(serviceType, decoratorType, decoratorFactory: null);
 
-    internal static DecorationStrategy WithFactory(Type serviceType, Func<object, IServiceProvider, object> decoratorFactory) => 
+    internal static DecorationStrategy WithFactory(Type serviceType, Func<object, System.IServiceProvider, object> decoratorFactory) =>
         Create(serviceType, decoratorType: null, decoratorFactory);
-    
-    protected static Func<IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
+
+    protected static Func<System.IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
     {
         var instanceToDecorate = serviceProvider.GetRequiredService(serviceType);
         return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, instanceToDecorate);
     };
 
-    protected static Func<IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, IServiceProvider, object> decoratorFactory) => serviceProvider =>
+    protected static Func<System.IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, System.IServiceProvider, object> decoratorFactory) => serviceProvider =>
     {
         var instanceToDecorate = serviceProvider.GetRequiredService(decorated);
         return decoratorFactory(instanceToDecorate, serviceProvider);
     };
 
-    private static DecorationStrategy Create(Type serviceType, Type? decoratorType, Func<object, IServiceProvider, object>? decoratorFactory)
+    private static DecorationStrategy Create(Type serviceType, Type? decoratorType, Func<object, System.IServiceProvider, object>? decoratorFactory)
     {
         if (serviceType.IsOpenGeneric())
         {
