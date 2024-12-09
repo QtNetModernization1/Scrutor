d3a1bc31032291fd26ab3d5e0444caec6a286c68
NoReply Amazon <no-reply@amazon.com>
2024-12-09 18:25:57 +0000

    [Commit] Iteration 194 #errors = 294 <= 287.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS1929][23,16] 'IEnumerable<ServiceDescriptor>' does not contain a definition for 'Decorate' and the best extension method overload 'ServiceCollectionExtensions.Decorate(IServiceCollection, Type, Type)' requires a receiver of type 'Microsoft.Extensions.DependencyInjection.IServiceCollection'``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -15,7 +15,7 @@ public static partial class ServiceCollectionExtensions
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
-    public static System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> Decorate<TService, TDecorator>(this System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services)
+    public static IServiceCollection Decorate<TService, TDecorator>(this IServiceCollection services)
         where TDecorator : TService
     {
         Preconditions.NotNull(services, nameof(services));
