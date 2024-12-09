9c77f5bf321b6a010f9acb3de10cfc5adf0f3f06
NoReply Amazon <no-reply@amazon.com>
2024-12-09 21:09:28 +0000

    Revert "[Commit] Iteration 470 #errors = 292 <= 288."
    This reverts commit 09ff38cfefe77d16b57d010adb7c2e278ac53fd6.

--- src/Scrutor/ServiceCollectionExtensions.cs
+++ src/Scrutor/ServiceCollectionExtensions.cs
@@ -2,18 +2,12 @@ using System;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Scrutor
-{
-    internal static class ServiceCollectionExtensions
-    {
-        public static bool HasRegistration(this IServiceCollection services, Type serviceType)
-        {
-            if (services == null)
-                throw new ArgumentNullException(nameof(services));
-            if (serviceType == null)
-                throw new ArgumentNullException(nameof(serviceType));
+namespace Scrutor;
 
-            return services.Any(x => x.ServiceType == serviceType);
-        }
+internal static class ServiceCollectionExtensions
+{
+    public static bool HasRegistration(this IServiceCollection services, Type serviceType)
+    {
+        return services.Any(x => x.ServiceType == serviceType);
     }
 }
