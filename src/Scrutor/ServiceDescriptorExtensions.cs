21405ac70eef693425ce9e5783c420ad13cbe7b1
NoReply Amazon <no-reply@amazon.com>
2024-12-09 18:22:26 +0000

    [Commit] Iteration 187 #errors = 287 <= 287.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceDescriptorExtensions.cs][CS0012][9,71] The type 'Enum' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -5,8 +5,8 @@ namespace Scrutor;
 
 internal static class ServiceDescriptorExtensions
 {
-    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) => 
-        new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
+    public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) =>
+        new ServiceDescriptor(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
     public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
