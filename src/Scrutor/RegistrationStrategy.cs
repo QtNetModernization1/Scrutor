cdc42ecf1875516e75a66e195185de44bc09f2cd
NoReply Amazon <no-reply@amazon.com>
2024-12-09 21:44:53 +0000

    [Commit] Iteration 531 #errors = 288 <= 288.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][54,22] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -50,8 +50,10 @@ public abstract class RegistrationStrategy
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) =>
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
+        {
             services.TryAdd(descriptor);
+        }
     }
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
