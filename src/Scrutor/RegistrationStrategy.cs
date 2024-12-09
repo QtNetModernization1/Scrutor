0d613090d8abc447d091fd2aa2c9a071eb6c1af3
NoReply Amazon <no-reply@amazon.com>
2024-12-09 20:33:25 +0000

    Revert "[Commit] Iteration 408 #errors = 290 <= 288."
    This reverts commit b260820590f5566d31f0a1e964108e19e795169c.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -49,8 +49,8 @@ public abstract class RegistrationStrategy
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor) =>
-            ((IServiceCollection)services).TryAdd(descriptor);
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) =>
+            services.TryAdd(descriptor);
     }
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
