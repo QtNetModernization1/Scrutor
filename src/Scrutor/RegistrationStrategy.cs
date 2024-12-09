8523c6c426c53655c8c0c1ac02d3f57612b52efd
NoReply Amazon <no-reply@amazon.com>
2024-12-09 21:52:27 +0000

    [Commit] Iteration 544 #errors = 288 <= 285.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][56,22] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -3,109 +3,107 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
 
-namespace Scrutor;
-
-using System.Collections.Generic;
-
-public abstract class RegistrationStrategy
+namespace Scrutor
 {
-    /// <summary>
-    /// Skips registrations for services that already exists.
-    /// </summary>
-    public static readonly RegistrationStrategy Skip = new SkipRegistrationStrategy();
+    public abstract class RegistrationStrategy
+    {
+        /// <summary>
+        /// Skips registrations for services that already exists.
+        /// </summary>
+        public static readonly RegistrationStrategy Skip = new SkipRegistrationStrategy();
 
-    /// <summary>
-    /// Appends a new registration for existing services.
-    /// </summary>
-    public static readonly RegistrationStrategy Append = new AppendRegistrationStrategy();
+        /// <summary>
+        /// Appends a new registration for existing services.
+        /// </summary>
+        public static readonly RegistrationStrategy Append = new AppendRegistrationStrategy();
 
-    /// <summary>
-    /// Throws when trying to register an existing service.
-    /// </summary>
-    public static readonly RegistrationStrategy Throw = new ThrowRegistrationStrategy();
+        /// <summary>
+        /// Throws when trying to register an existing service.
+        /// </summary>
+        public static readonly RegistrationStrategy Throw = new ThrowRegistrationStrategy();
 
-    /// <summary>
+        /// <summary>
 /// Replaces existing service registrations using <see cref="ReplacementBehavior.Default"/>.
-    /// </summary>
-    public static RegistrationStrategy Replace()
-    {
-        return Replace(ReplacementBehavior.Default);
-    }
-
-    /// <summary>
-    /// Replaces existing service registrations based on the specified <see cref="ReplacementBehavior"/>.
-    /// </summary>
-    /// <param name="behavior">The behavior to use when replacing services.</param>
-    public static RegistrationStrategy Replace(ReplacementBehavior behavior)
-    {
-        return new ReplaceRegistrationStrategy(behavior);
-    }
-
-    /// <summary>
-    /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/>.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
-
-    private sealed class SkipRegistrationStrategy : RegistrationStrategy
-    {
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) =>
-            services.TryAdd(descriptor);
-    }
-
-    private sealed class AppendRegistrationStrategy : RegistrationStrategy
-    {
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
+        /// </summary>
+        public static RegistrationStrategy Replace()
         {
-            ((IList<ServiceDescriptor>)services).Add(descriptor);
-        }
-    }
-
-    private sealed class ThrowRegistrationStrategy : RegistrationStrategy
-    {
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
-        {
-            if (((IEnumerable<ServiceDescriptor>)services).Any(s => s.ServiceType == descriptor.ServiceType))
-            {
-                throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
-            }
-
-            services.Add(descriptor);
-        }
-    }
-
-    private sealed class ReplaceRegistrationStrategy : RegistrationStrategy
-    {
-        public ReplaceRegistrationStrategy(ReplacementBehavior behavior)
-        {
-            Behavior = behavior;
+            return Replace(ReplacementBehavior.Default);
         }
 
-        private ReplacementBehavior Behavior { get; }
-
-        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
+        /// <summary>
+        /// Replaces existing service registrations based on the specified <see cref="ReplacementBehavior"/>.
+        /// </summary>
+        /// <param name="behavior">The behavior to use when replacing services.</param>
+        public static RegistrationStrategy Replace(ReplacementBehavior behavior)
         {
-            var behavior = Behavior;
+            return new ReplaceRegistrationStrategy(behavior);
+        }
 
-            if (behavior == ReplacementBehavior.Default)
+        /// <summary>
+        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="descriptor">The descriptor to apply.</param>
+        public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
+
+        private sealed class SkipRegistrationStrategy : RegistrationStrategy
+        {
+            public override void Apply(IServiceCollection services, ServiceDescriptor descriptor) =>
+                services.TryAdd(descriptor);
+        }
+
+        private sealed class AppendRegistrationStrategy : RegistrationStrategy
+        {
+            public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
             {
-                behavior = ReplacementBehavior.ServiceType;
+                services.Add(descriptor);
+            }
+        }
+
+        private sealed class ThrowRegistrationStrategy : RegistrationStrategy
+        {
+            public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
+            {
+                if (services.Any(s => s.ServiceType == descriptor.ServiceType))
+                {
+                    throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
+                }
+
+                services.Add(descriptor);
+            }
+        }
+
+        private sealed class ReplaceRegistrationStrategy : RegistrationStrategy
+        {
+            public ReplaceRegistrationStrategy(ReplacementBehavior behavior)
+            {
+                Behavior = behavior;
             }
 
-            if (behavior.HasFlag(ReplacementBehavior.ServiceType))
-            {
-                services.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
-            }
+            private ReplacementBehavior Behavior { get; }
 
-            if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
+            public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
             {
-                services.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
-            }
+                var behavior = Behavior;
 
-            services.Add(descriptor);
+                if (behavior == ReplacementBehavior.Default)
+                {
+                    behavior = ReplacementBehavior.ServiceType;
+                }
+
+                if (behavior.HasFlag(ReplacementBehavior.ServiceType))
+                {
+                    services.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
+                }
+
+                if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
+                {
+                    services.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
+                }
+
+                services.Add(descriptor);
+            }
         }
     }
 }
