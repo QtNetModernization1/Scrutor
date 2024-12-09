742dc4429cac07c68ba37fa643b1a1a3bffa160d
NoReply Amazon <no-reply@amazon.com>
2024-12-09 22:13:48 +0000

    [Commit] Iteration 579 #errors = 284 <= 284.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][59,22] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -5,10 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
 
-#if NETSTANDARD2_0 || NET461
-// Removed duplicate using directive
-#endif
-
 namespace Scrutor;
 
 public abstract class RegistrationStrategy
