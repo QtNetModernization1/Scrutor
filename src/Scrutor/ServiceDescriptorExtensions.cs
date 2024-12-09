20290a0414f26f357bfba62e098d75c466d6b13b
NoReply Amazon <no-reply@amazon.com>
2024-12-09 22:03:30 +0000

    Revert "[Commit] Iteration 563 #errors = 285 <= 285."
    This reverts commit c340b2341fb124f256adb622f6f74989a4892373.

--- src/Scrutor/ServiceDescriptorExtensions.cs
+++ src/Scrutor/ServiceDescriptorExtensions.cs
@@ -7,10 +7,10 @@ namespace Scrutor;
 internal static class ServiceDescriptorExtensions
 {
     public static ServiceDescriptor WithImplementationFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> implementationFactory) =>
-        new ServiceDescriptor(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
+        new(descriptor.ServiceType, implementationFactory, descriptor.Lifetime);
 
 #if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER || NET6_0_OR_GREATER || NET461
-    public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, System.Type serviceType) => descriptor switch
+    public static ServiceDescriptor WithServiceType(this ServiceDescriptor descriptor, Type serviceType) => descriptor switch
     {
         { ImplementationType: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationType, descriptor.Lifetime),
         { ImplementationFactory: not null } => new ServiceDescriptor(serviceType, descriptor.ImplementationFactory, descriptor.Lifetime),
