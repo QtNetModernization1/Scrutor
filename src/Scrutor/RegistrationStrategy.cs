5e04d5dcf2aaac576e463cfddaa57d2f767481f9
NoReply Amazon <no-reply@amazon.com>
2024-12-10 01:03:05 +0000

    [Commit] Iteration 858 #errors = 290 <= 305.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][57,35] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -1,9 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
 
 namespace Scrutor;
 
@@ -46,55 +44,34 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor);
+    public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services is IServiceCollection serviceCollection)
-            {
-                serviceCollection.TryAdd(descriptor);
-            }
-            else
-            {
-                throw new ArgumentException("Services must be an instance of IServiceCollection", nameof(services));
-            }
+            services.TryAdd(descriptor);
         }
     }
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services is IServiceCollection serviceCollection)
-            {
-                serviceCollection.Add(descriptor);
-            }
-            else
-            {
-                throw new ArgumentException("Services must be an instance of IServiceCollection", nameof(services));
-            }
+            services.Add(descriptor);
         }
     }
 
     private sealed class ThrowRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services is IServiceCollection serviceCollection)
+            if (services.Any(s => s.ServiceType == descriptor.ServiceType))
             {
-                if (serviceCollection.Any(s => s.ServiceType == descriptor.ServiceType))
-                {
-                    throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
-                }
+                throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
+            }
 
-                serviceCollection.Add(descriptor);
-            }
-            else
-            {
-                throw new ArgumentException("Services must be an instance of IServiceCollection", nameof(services));
-            }
+            services.Add(descriptor);
         }
     }
 
@@ -107,13 +84,8 @@ public abstract class RegistrationStrategy
 
         private ReplacementBehavior Behavior { get; }
 
-        public override void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services is not IServiceCollection serviceCollection)
-            {
-                throw new ArgumentException("Services must be an instance of IServiceCollection", nameof(services));
-            }
-
             var behavior = Behavior;
 
             if (behavior == ReplacementBehavior.Default)
@@ -123,15 +95,15 @@ public abstract class RegistrationStrategy
 
             if (behavior.HasFlag(ReplacementBehavior.ServiceType))
             {
-                serviceCollection.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
+                services.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
             }
 
             if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
             {
-                serviceCollection.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
+                services.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
             }
 
-            serviceCollection.Add(descriptor);
+            services.Add(descriptor);
         }
     }
 }
