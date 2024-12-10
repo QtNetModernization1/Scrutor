0994aa4eee302bfca4db9b68fb6b77529c70f785
NoReply Amazon <no-reply@amazon.com>
2024-12-10 01:38:45 +0000

    Revert "[Commit] Iteration 924 #errors = 297 <= 297."
    This reverts commit 11b02225c37c9b7800a7b93fb3b55eec48506622.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -63,7 +62,7 @@ namespace Scrutor;
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            ((ICollection<ServiceDescriptor>)services).Add(descriptor);
+            ((System.Collections.Generic.ICollection<ServiceDescriptor>)services).Add(descriptor);
         }
     }
 
