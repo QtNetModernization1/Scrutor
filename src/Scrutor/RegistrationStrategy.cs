6f59c43bf7d10927885e217b79a06499cf8e6691
NoReply Amazon <no-reply@amazon.com>
2024-12-10 03:05:33 +0000

    [Commit] Iteration 1074 #errors = 298 <= 301.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0115][94,30] 'ReplaceRegistrationStrategy.Apply(IServiceCollection, ServiceDescriptor)': no suitable method found to override``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -43,16 +43,16 @@ namespace Scrutor
         }
 
         /// <summary>
-        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="System.Collections.Generic.ICollection{T}"/> of <see cref="ServiceDescriptor"/>.
+        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/> of <see cref="ServiceDescriptor"/>.
         /// </summary>
         /// <param name="services">The service collection.</param>
         /// <param name="descriptor">The descriptor to apply.</param>
-        public abstract void Apply(System.Collections.Generic.ICollection<ServiceDescriptor> services, ServiceDescriptor descriptor);
+        public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
     }
 
     internal sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.ICollection<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
             if (!services.Any(s => s.ServiceType == descriptor.ServiceType))
             {
@@ -63,7 +63,7 @@ namespace Scrutor
 
     internal sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.ICollection<ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
             services.Add(descriptor);
         }
