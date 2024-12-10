148918021ee04f16e5fccc334587237f6efd62c8
NoReply Amazon <no-reply@amazon.com>
2024-12-10 02:02:09 +0000

    [Commit] Iteration 964 #errors = 298 <= 321.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0115][53,30] 'RegistrationStrategy.SkipRegistrationStrategy.Apply(IServiceCollection, ServiceDescriptor)': no suitable method found to override``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -42,11 +42,11 @@ namespace Scrutor;
         }
 
         /// <summary>
-        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="System.Collections.Generic.IList{T}"/> of <see cref="ServiceDescriptor"/>.
+        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/> of <see cref="ServiceDescriptor"/>.
         /// </summary>
         /// <param name="services">The service collection.</param>
         /// <param name="descriptor">The descriptor to apply.</param>
-        public abstract void Apply(System.Collections.Generic.IList<ServiceDescriptor> services, ServiceDescriptor descriptor);
+        public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
