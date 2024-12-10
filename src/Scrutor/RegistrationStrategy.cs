e76ad3592630cff508aca96a5666e71a5c0ee9ad
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:29:29 +0000

    [Commit] Iteration 801 #errors = 301 <= 301.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][136,31] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -8,6 +8,7 @@ using System.Collections;
 namespace Scrutor;
 
 using IEnumerableOfServiceDescriptor = System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
+using IServiceCollection = Microsoft.Extensions.DependencyInjection.IServiceCollection;
 
 public abstract class RegistrationStrategy
 {
