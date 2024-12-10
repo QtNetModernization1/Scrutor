35c22174ebef90de5756903e92f3909dd019c9a1
NoReply Amazon <no-reply@amazon.com>
2024-12-10 02:04:06 +0000

    [Commit] Iteration 968 #errors = 299 <= 298.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][66,22] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic; // Explicitly added to ensure IList<> is recognized
 
 namespace Scrutor;
 
