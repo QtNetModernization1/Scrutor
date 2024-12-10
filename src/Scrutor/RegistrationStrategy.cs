99dbc9dd6c0a6352f4f1509319231fd11fcfd685
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:36:13 +0000

    [Commit] Iteration 812 #errors = 301 <= 301.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][136,31] The type 'IEnumerable' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -8,6 +8,7 @@ using System.Collections;
 namespace Scrutor;
 
 using IEnumerableOfServiceDescriptor = System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
+using System.Collections.Generic;
 
 public abstract class RegistrationStrategy
 {
@@ -48,7 +49,7 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor);
+    public abstract void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
