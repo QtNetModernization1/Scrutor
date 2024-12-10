736ce88a3d620044cf6b0deaff8c2e3dc9cfbb9b
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:27:25 +0000

    [Commit] Iteration 797 #errors = 290 <= 301.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][136,31] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -111,7 +111,7 @@ public abstract class RegistrationStrategy
 
         public override void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor)
         {
-            if (services is not IServiceCollection serviceCollection)
+            if (services is not System.Collections.Generic.IList<ServiceDescriptor> serviceCollection)
             {
                 throw new ArgumentException("Services must be an instance of IServiceCollection", nameof(services));
             }
@@ -125,12 +125,24 @@ public abstract class RegistrationStrategy
 
             if (behavior.HasFlag(ReplacementBehavior.ServiceType))
             {
-                serviceCollection.RemoveAll(s => s.ServiceType == descriptor.ServiceType);
+                for (int i = serviceCollection.Count - 1; i >= 0; i--)
+                {
+                    if (serviceCollection[i].ServiceType == descriptor.ServiceType)
+                    {
+                        serviceCollection.RemoveAt(i);
+                    }
+                }
             }
 
             if (behavior.HasFlag(ReplacementBehavior.ImplementationType))
             {
-                serviceCollection.RemoveAll(s => s.ImplementationType == descriptor.ImplementationType);
+                for (int i = serviceCollection.Count - 1; i >= 0; i--)
+                {
+                    if (serviceCollection[i].ImplementationType == descriptor.ImplementationType)
+                    {
+                        serviceCollection.RemoveAt(i);
+                    }
+                }
             }
 
             serviceCollection.Add(descriptor);
