0e4b36d1cebd322c2e2aa97d26537ae1c5b9ec1e
NoReply Amazon <no-reply@amazon.com>
2024-12-10 01:17:47 +0000

    [Commit] Iteration 882 #errors = 298 <= 305.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][72,35] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -7,46 +7,46 @@ using System.Collections;
 
 namespace Scrutor;
 
-public abstract class RegistrationStrategy
-{
-    /// <summary>
-    /// Skips registrations for services that already exists.
-    /// </summary>
-    public static readonly RegistrationStrategy Skip = new SkipRegistrationStrategy();
+    public abstract class RegistrationStrategy
+    {
+        /// <summary>
+        /// Skips registrations for services that already exists.
+        /// </summary>
+        public static readonly RegistrationStrategy Skip = new SkipRegistrationStrategy();
 
-    /// <summary>
-    /// Appends a new registration for existing services.
-    /// </summary>
-    public static readonly RegistrationStrategy Append = new AppendRegistrationStrategy();
+        /// <summary>
+        /// Appends a new registration for existing services.
+        /// </summary>
+        public static readonly RegistrationStrategy Append = new AppendRegistrationStrategy();
 
-    /// <summary>
-    /// Throws when trying to register an existing service.
-    /// </summary>
-    public static readonly RegistrationStrategy Throw = new ThrowRegistrationStrategy();
+        /// <summary>
+        /// Throws when trying to register an existing service.
+        /// </summary>
+        public static readonly RegistrationStrategy Throw = new ThrowRegistrationStrategy();
 
-    /// <summary>
+        /// <summary>
 /// Replaces existing service registrations using <see cref="ReplacementBehavior.Default"/>.
-    /// </summary>
-    public static RegistrationStrategy Replace()
-    {
-        return Replace(ReplacementBehavior.Default);
-    }
+        /// </summary>
+        public static RegistrationStrategy Replace()
+        {
+            return Replace(ReplacementBehavior.Default);
+        }
 
-    /// <summary>
-    /// Replaces existing service registrations based on the specified <see cref="ReplacementBehavior"/>.
-    /// </summary>
-    /// <param name="behavior">The behavior to use when replacing services.</param>
-    public static RegistrationStrategy Replace(ReplacementBehavior behavior)
-    {
-        return new ReplaceRegistrationStrategy(behavior);
-    }
+        /// <summary>
+        /// Replaces existing service registrations based on the specified <see cref="ReplacementBehavior"/>.
+        /// </summary>
+        /// <param name="behavior">The behavior to use when replacing services.</param>
+        public static RegistrationStrategy Replace(ReplacementBehavior behavior)
+        {
+            return new ReplaceRegistrationStrategy(behavior);
+        }
 
-    /// <summary>
-    /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/>.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(IEnumerable<ServiceDescriptor> services, ServiceDescriptor descriptor);
+        /// <summary>
+        /// Applies the <see cref="ServiceDescriptor"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="descriptor">The descriptor to apply.</param>
+        public abstract void Apply(IServiceCollection services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
