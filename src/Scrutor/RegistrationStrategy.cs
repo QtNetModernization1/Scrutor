8aec2d9535dda48f68af90c9d3a3479916fc3d46
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:40:41 +0000

    Revert "[Commit] Iteration 819 #errors = 301 <= 301."
    This reverts commit a09396db24ec89694c9a922fa1f9456f0f9653ad.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -11,8 +11,6 @@ using IEnumerableOfServiceDescriptor = System.Collections.Generic.IEnumerable<Mi
 
 public abstract class RegistrationStrategy
 {
-    private static readonly System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> EmptyDescriptors =
-        new System.Collections.Generic.List<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>();
     /// <summary>
     /// Skips registrations for services that already exists.
     /// </summary>
@@ -50,11 +48,11 @@ public abstract class RegistrationStrategy
     /// </summary>
     /// <param name="services">The service collection.</param>
     /// <param name="descriptor">The descriptor to apply.</param>
-    public abstract void Apply(System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, ServiceDescriptor descriptor);
+    public abstract void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor);
 
     private sealed class SkipRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor)
         {
             if (services is IServiceCollection serviceCollection)
             {
@@ -69,7 +67,7 @@ public abstract class RegistrationStrategy
 
     private sealed class AppendRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor)
         {
             if (services is IServiceCollection serviceCollection)
             {
@@ -84,7 +82,7 @@ public abstract class RegistrationStrategy
 
     private sealed class ThrowRegistrationStrategy : RegistrationStrategy
     {
-        public override void Apply(System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor)
         {
             if (services is IServiceCollection serviceCollection)
             {
@@ -111,7 +109,7 @@ public abstract class RegistrationStrategy
 
         private ReplacementBehavior Behavior { get; }
 
-        public override void Apply(System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, ServiceDescriptor descriptor)
+        public override void Apply(IEnumerableOfServiceDescriptor services, ServiceDescriptor descriptor)
         {
             if (services is not IServiceCollection serviceCollection)
             {
