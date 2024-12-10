d477dd73aaed314bc733c311abc71f1cc1226a51
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:26:52 +0000

    Revert "[Commit] Iteration 796 #errors = 302 <= 301."
    This reverts commit acae4c2c55a611a0da9bab17fd77ba62d2de9a1b.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -8,7 +8,6 @@ using System.Collections;
 namespace Scrutor;
 
 using IEnumerableOfServiceDescriptor = System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor>;
-using IList = System.Collections.Generic.IList<>;
 
 public abstract class RegistrationStrategy
 {
