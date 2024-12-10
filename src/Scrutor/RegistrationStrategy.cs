be6b0399e6bae4a752f7ed8b0669fefb9f96c455
NoReply Amazon <no-reply@amazon.com>
2024-12-10 02:29:23 +0000

    [Commit] Iteration 1013 #errors = 296 <= 298.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][56,27] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -53,7 +53,7 @@ namespace Scrutor
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (!services.Any(s => s.ServiceType == descriptor.ServiceType))
+            if (!((System.Collections.Generic.IEnumerable<ServiceDescriptor>)services).Any(s => s.ServiceType == descriptor.ServiceType))
             {
                 services.Add(descriptor);
             }
