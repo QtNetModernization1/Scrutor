6f7452c99297cd9f5c10b24d21a9f2e011714e42
NoReply Amazon <no-reply@amazon.com>
2024-12-10 00:52:11 +0000

    Revert "[Commit] Iteration 840 #errors = 301 <= 301."
    This reverts commit c7fc679a7a4b87926f04436bab6fa043650d28c8.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,7 +1,6 @@
 using Scrutor;
 using System;
 using JetBrains.Annotations;
-using Microsoft.Extensions.DependencyInjection.Abstractions;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
