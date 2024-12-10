d94b6e6312bcdead5f3cb5ba67ae4d03d652fb52
NoReply Amazon <no-reply@amazon.com>
2024-12-10 02:15:30 +0000

    [Commit] Iteration 988 #errors = 296 <= 299.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/RegistrationStrategy.cs][CS0012][56,27] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -53,7 +53,7 @@ namespace Scrutor
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (!services.AsEnumerable().Any(s => s.ServiceType == descriptor.ServiceType))
+            if (!((IEnumerable<ServiceDescriptor>)services).Any(s => s.ServiceType == descriptor.ServiceType))
             {
                 services.Add(descriptor);
             }
