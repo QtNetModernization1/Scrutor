2e0884bd76e2ca81208784673e1e58f4a8b496f3
NoReply Amazon <no-reply@amazon.com>
2025-01-16 20:12:25 +0000

    Revert "[Commit] Iteration 284 #errors = 218 <= 217."
    This reverts commit 776e638b5f5763bd79701bba47d1eeb06c9fd4c1.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,11 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
-
-// Explicitly import System namespace to ensure Enum is available
-using System;
 
 namespace Scrutor;
 
