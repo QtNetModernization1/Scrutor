42eb09ea0ad5c58b3f2826a12cdbab6bb23406af
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:58:09 +0000

    Revert "[Commit] Iteration 92 #errors = 218 <= 218."
    This reverts commit 09116d319ae73e38c037b3eea75e21ff0b287095.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,6 +1,5 @@
-using Scrutor;
+﻿using Scrutor;
 using System;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -33,10 +32,7 @@ public static partial class ServiceCollectionExtensions
     public static bool TryDecorate<TService, TDecorator>(this IServiceCollection services)
         where TDecorator : TService
     {
-        if (services == null)
-        {
-            throw new ArgumentNullException(nameof(services));
-        }
+        Preconditions.NotNull(services, nameof(services));
 
         return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
