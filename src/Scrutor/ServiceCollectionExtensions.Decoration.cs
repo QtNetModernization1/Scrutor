6bc61f503751ae67d0e45f3f5e079074b16ba69c
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:49:59 +0000

    [Commit] Iteration 81 #errors = 218 <= 218.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][35,23] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,5 +1,6 @@
-﻿using Scrutor;
+using Scrutor;
 using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -10,7 +11,7 @@ public static partial class ServiceCollectionExtensions
 {
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
-    /// using the specified type <typeparamref name="TDecorator"/>.
+/// using the specified type <typeparamref name="TDecorator"/>.
     /// </summary>
     /// <param name="services">The services to add to.</param>
     /// <exception cref="DecorationException">If no service of the type <typeparamref name="TService"/> has been registered.</exception>
@@ -25,14 +26,17 @@ public static partial class ServiceCollectionExtensions
 
     /// <summary>
     /// Decorates all registered services of type <typeparamref name="TService"/>
-    /// using the specified type <typeparamref name="TDecorator"/>.
+/// using the specified type <typeparamref name="TDecorator"/>.
     /// </summary>
     /// <param name="services">The services to add to.</param>
     /// <exception cref="ArgumentNullException">If the <paramref name="services"/> argument is <c>null</c>.</exception>
     public static bool TryDecorate<TService, TDecorator>(this IServiceCollection services)
         where TDecorator : TService
     {
-        Preconditions.NotNull(services, nameof(services));
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
 
         return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
