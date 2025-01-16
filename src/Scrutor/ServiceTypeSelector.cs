ad9be1bc2a2a67dfc2ec03e14b06a7c686420ea0
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:15:43 +0000

    Revert "[Commit] Iteration 35 #errors = 218 <= 218."
    This reverts commit 3e67a8d9a99a99101e484b39d12cf4cc075fe5ff.

--- src/Scrutor/ServiceTypeSelector.cs
+++ src/Scrutor/ServiceTypeSelector.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
-using System.Runtime.InteropServices;
 
 namespace Scrutor;
 
