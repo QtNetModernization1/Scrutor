1f30ae530e12cecae7674ae33f2b4e449e7dad39
NoReply Amazon <no-reply@amazon.com>
2025-01-16 20:51:41 +0000

    [Commit] Iteration 342 #errors = 217 <= 217.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/LifetimeSelector.cs][CS0453][43,23] The type 'ServiceLifetime' must be a non-nullable value type in order to use it as parameter 'TEnum' in the generic type or method 'Preconditions.IsDefined<TEnum>(TEnum, string)'``.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Abstractions;
 using Microsoft.Extensions.DependencyModel;
 
 namespace Scrutor;
