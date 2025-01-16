be3ae5a17703c0c0c0d315903d8c599c1dad1427
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:05:58 +0000

    [Commit] Iteration 22 #errors = 224 <= 218.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceDescriptorAttribute.cs][CS0012][24,12] The type 'Enum' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceDescriptorAttribute.cs
+++ src/Scrutor/ServiceDescriptorAttribute.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.Versioning;
 
 namespace Scrutor;
 
@@ -21,7 +22,11 @@ public class ServiceDescriptorAttribute : Attribute
 
     public Type? ServiceType { get; }
 
-    public ServiceLifetime Lifetime { get; }
+public ServiceLifetime Lifetime { get; }
+
+[SupportedOSPlatform("windows")]
+[SupportedOSPlatform("linux")]
+[SupportedOSPlatform("macOS")]
 
     public IEnumerable<Type> GetServiceTypes(Type fallbackType)
     {
