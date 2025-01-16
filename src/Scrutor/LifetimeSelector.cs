a09e715ba8658cd4b2e63f7c5d474b9adb25f813
NoReply Amazon <no-reply@amazon.com>
2025-01-16 20:50:59 +0000

    [Commit] Iteration 340 #errors = 218 <= 217.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/LifetimeSelector.cs][CS0012][38,45] The type 'Enum' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
+using System;
 
 namespace Scrutor;
 
