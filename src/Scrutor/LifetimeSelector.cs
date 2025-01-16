1cb8e79440c4bc1fb280f942af77ba4fa039141b
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:02:39 +0000

    Revert "[Commit] Iteration 17 #errors = 219 <= 218."
    This reverts commit db961659410cda8df6722faac7b640ad3df0b698.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -1,11 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
-#if NETSTANDARD2_0 || NET461
-using System.Enum;
-#endif
 
 namespace Scrutor;
 
