82ef2461021482593d27bb4a1b22ea4add45d3cd
NoReply Amazon <no-reply@amazon.com>
2025-01-16 19:16:20 +0000

    Revert "[Commit] Iteration 201 #errors = 219 <= 217."
    This reverts commit 3af982f2eac0717e82bbfa8877ab05761662f2c2.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,6 +1,5 @@
-using Scrutor;
+﻿using Scrutor;
 using System;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -54,7 +53,7 @@ public static partial class ServiceCollectionExtensions
         Preconditions.NotNull(serviceType, nameof(serviceType));
         Preconditions.NotNull(decoratorType, nameof(decoratorType));
 
-        return services.Decorate(new DecorationStrategy(serviceType, decoratorType));
+        return services.Decorate(DecorationStrategy.WithType(serviceType, decoratorType));
     }
 
     /// <summary>
@@ -72,7 +71,7 @@ public static partial class ServiceCollectionExtensions
         Preconditions.NotNull(serviceType, nameof(serviceType));
         Preconditions.NotNull(decoratorType, nameof(decoratorType));
 
-        return services.TryDecorate(new DecorationStrategy(serviceType, decoratorType));
+        return services.TryDecorate(DecorationStrategy.WithType(serviceType, decoratorType));
     }
     
     /// <summary>
