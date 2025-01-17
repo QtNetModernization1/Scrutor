fef1905886ec34067aaf48a924a8f92bbef2e696
NoReply Amazon <no-reply@amazon.com>
2025-01-17 03:33:53 +0000

    [Commit] Iteration 949 #errors = 225 <= 225.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][39,25] The type 'IEnumerable<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,9 +3,11 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using System.Collections;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
+
 
 [PublicAPI]
 public static partial class ServiceCollectionExtensions
@@ -40,6 +42,9 @@ public static partial class ServiceCollectionExtensions
     }
 
     private static IEnumerable<T> AsEnumerable<T>(this IServiceCollection services) => services.Cast<T>();
+
+    // Add this method to ensure IEnumerable<T> is available
+    private static IEnumerable<T> EnsureEnumerable<T>(this IEnumerable<T> source) => source;
 
     /// <summary>
     /// Decorates all registered services of the specified <paramref name="serviceType"/>
