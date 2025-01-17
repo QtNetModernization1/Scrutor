c015437e574aa8b7719f0f776bf7d73e69e3e836
NoReply Amazon <no-reply@amazon.com>
2025-01-17 04:15:23 +0000

    [Commit] Iteration 1022 #errors = 220 <= 217.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceDescriptorAttribute.cs][CS0012][24,53] The type 'Enum' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceDescriptorAttribute.cs
+++ src/Scrutor/ServiceDescriptorAttribute.cs
@@ -55,9 +55,9 @@ public class ServiceDescriptorAttribute : Attribute
 
 [PublicAPI]
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class ServiceDescriptorAttribute<TService> : ServiceDescriptorAttribute
+public sealed class ServiceDescriptorAttribute<TService> : ServiceDescriptorAttribute where TService : class
 {
     public ServiceDescriptorAttribute() : base(typeof(TService)) { }
 
-    public ServiceDescriptorAttribute(ServiceLifetime lifetime) : base(typeof(TService), lifetime) { }
+    public ServiceDescriptorAttribute(System.Enum lifetime) : base(typeof(TService), (Microsoft.Extensions.DependencyInjection.ServiceLifetime)lifetime) { }
 }
