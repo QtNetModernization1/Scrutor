9abfa76adfea6065909a584c48776dae168b1f12
NoReply Amazon <no-reply@amazon.com>
2025-01-17 02:54:00 +0000

    Revert "[Commit] Iteration 892 #errors = 225 <= 225."
    This reverts commit fed6a505633f9022a5821803217c7a73b0a93626.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -2,7 +2,6 @@ using Scrutor;
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
