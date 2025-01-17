75fa2d6302e09e27489565ad570c334b8c2f2afe
NoReply Amazon <no-reply@amazon.com>
2025-01-17 03:38:13 +0000

    [Commit] Iteration 956 #errors = 225 <= 225.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][42,97] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -2,6 +2,7 @@ using Scrutor;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
