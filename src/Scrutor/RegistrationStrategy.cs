1c13694a11cc177921f555517f34315bc61fd87e
NoReply Amazon <no-reply@amazon.com>
2025-01-17 01:14:42 +0000

    Revert "[Commit] Iteration 740 #errors = 211 <= 215."
    This reverts commit f39e90abec7d20ab2838ba80447c230e7199e2f0.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -58,7 +58,7 @@ public abstract class RegistrationStrategy
     {
         public override void Apply(IServiceCollection services, ServiceDescriptor descriptor)
         {
-            if (services.Any(s => s.ServiceType == descriptor.ServiceType))
+            if (services.HasRegistration(descriptor.ServiceType))
             {
                 throw new DuplicateTypeRegistrationException(descriptor.ServiceType);
             }
