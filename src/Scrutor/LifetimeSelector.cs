6d49554c4c1314944d705521e9f73a0a53ff9652
NoReply Amazon <no-reply@amazon.com>
2025-01-17 00:25:42 +0000

    [Commit] Iteration 665 #errors = 215 <= 215.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/LifetimeSelector.cs][CS0012][219,38] The type 'Type' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
+using System.Runtime.InteropServices;
 
 namespace Scrutor;
 
