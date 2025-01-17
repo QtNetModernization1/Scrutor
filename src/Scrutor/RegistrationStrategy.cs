16f200ff49aa039f51522458a88b06c06d487a41
NoReply Amazon <no-reply@amazon.com>
2025-01-17 03:58:57 +0000

    Revert "[Commit] Iteration 993 #errors = 218 <= 218."
    This reverts commit 62bdd0463612da9905caee1997d1560e663d0efe.

--- src/Scrutor/RegistrationStrategy.cs
+++ src/Scrutor/RegistrationStrategy.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace Scrutor;
 
