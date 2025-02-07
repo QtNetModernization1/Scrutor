c45549e1d2574b2778bca6db9b7402ed6dd4f5ff
NoReply Amazon <no-reply@amazon.com>
2025-02-07 22:43:40 +0000

    Revert "[Commit] Iteration 31 #errors = 211 <= 211."
    This reverts commit ddd9d9e885fe14e9cd5ab07fb650a0237c2fc3c3.

--- src/Scrutor/LifetimeSelector.cs
+++ src/Scrutor/LifetimeSelector.cs
@@ -47,7 +47,7 @@ internal sealed class LifetimeSelector : ILifetimeSelector, ISelector
         return WithLifetime(ServiceLifetime.Transient);
     }
 
-    public ILifetimeSelector WithLifetime(ServiceLifetime lifetime)
+    public IImplementationTypeSelector WithLifetime(ServiceLifetime lifetime)
     {
         Preconditions.IsDefined(lifetime, nameof(lifetime));
 
