60e780fa448107eeab19e4eb8553de9b6874f1fe
NoReply Amazon <no-reply@amazon.com>
2025-02-08 05:18:27 +0000

    Revert "[Commit] Iteration 660 #errors = 211 <= 211."
    This reverts commit 6bf8779b43d1c6c4679e678aac9aa73639901c3f.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -5,10 +5,6 @@ using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 
-#if NET461
-using System.Collections;
-#endif
-
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
