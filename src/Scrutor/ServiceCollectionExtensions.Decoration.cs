e90cfc681a6d79cac6e1a3de62f3e6ec862d1217
NoReply Amazon <no-reply@amazon.com>
2025-02-08 03:47:52 +0000

    Revert "[Commit] Iteration 514 #errors = 223 <= 223."
    This reverts commit b4020c6479bd0f1de085c3d0084021791ca225c8.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using System.Collections;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -95,7 +94,7 @@ public static partial class ServiceCollectionExtensions
         return services.Decorate<TService>((service, _) => decorator(service));
     }
 
-    public static IServiceCollection Decorate<TService>(this IEnumerable<ServiceDescriptor> services, Func<TService, IServiceProvider, TService> decorator) where TService : notnull
+    public static IServiceCollection Decorate<TService>(this System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, Func<TService, System.IServiceProvider, TService> decorator) where TService : notnull
     {
         Preconditions.NotNull(services, nameof(services));
         Preconditions.NotNull(decorator, nameof(decorator));
