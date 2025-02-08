de35646b8363fe0b2324815facabe1bfc4ffc4ae
NoReply Amazon <no-reply@amazon.com>
2025-02-08 07:47:15 +0000

    [Commit] Iteration 902 #errors = 212 <= 212.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][41,25] The type 'IList<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -40,6 +41,8 @@ public static partial class ServiceCollectionExtensions
 
         return services.TryDecorate(typeof(TService), typeof(TDecorator));
     }
+
+    private static IList<T> CreateList<T>() => new List<T>();
 
     /// <summary>
     /// Decorates all registered services of the specified <paramref name="serviceType"/>
