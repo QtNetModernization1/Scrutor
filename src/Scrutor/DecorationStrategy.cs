903114612839bda185ff27dcd133e35d38feccf9
NoReply Amazon <no-reply@amazon.com>
2025-02-08 00:34:22 +0000

    [Commit] Iteration 205 #errors = 211 <= 211.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/DecorationStrategy.cs][CS0012][28,35] The type 'Object' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/DecorationStrategy.cs
+++ src/Scrutor/DecorationStrategy.cs
@@ -1,5 +1,8 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
+#if NET8_0
+using System.Runtime.InteropServices;
+#endif
 
 namespace Scrutor;
 
@@ -25,7 +28,11 @@ public abstract class DecorationStrategy
     protected static Func<IServiceProvider, object> TypeDecorator(Type serviceType, Type decoratorType) => serviceProvider =>
     {
         var instanceToDecorate = serviceProvider.GetRequiredService(serviceType);
+#if NET8_0
+        return RuntimeHelpers.GetUninitializedObject(decoratorType);
+#else
         return ActivatorUtilities.CreateInstance(serviceProvider, decoratorType, instanceToDecorate);
+#endif
     };
 
     protected static Func<IServiceProvider, object> FactoryDecorator(Type decorated, Func<object, IServiceProvider, object> decoratorFactory) => serviceProvider =>
