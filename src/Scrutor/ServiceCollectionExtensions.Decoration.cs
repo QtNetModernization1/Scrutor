764ebe471c03470b31f5022f3be1e033735a5eaf
NoReply Amazon <no-reply@amazon.com>
2025-02-08 09:13:37 +0000

    Revert "[Commit] Iteration 1043 #errors = 212 <= 212."
    This reverts commit 2b7d2ba42cf5f87d1741d50f899068a6b6607e85.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -5,9 +5,6 @@ using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections;
-#if NETSTANDARD2_0 || NET461
-using System.Collections;
-#endif
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -91,7 +88,7 @@ public static partial class ServiceCollectionExtensions
     /// <exception cref="DecorationException">If no service of <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If either the <paramref name="services"/>
     /// or <paramref name="decorator"/> arguments are <c>null</c>.</exception>
-    public static IServiceCollection Decorate<TService>(this IServiceCollection services, Func<TService, TService> decorator) where TService : class
+    public static IServiceCollection Decorate<TService>(this IServiceCollection services, Func<TService, TService> decorator) where TService : notnull
     {
         Preconditions.NotNull(services, nameof(services));
         Preconditions.NotNull(decorator, nameof(decorator));
