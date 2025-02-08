a07b3337f38904a76e049ce3dc5eadd0495e20f2
NoReply Amazon <no-reply@amazon.com>
2025-02-08 09:42:53 +0000

    Revert "[Commit] Iteration 1088 #errors = 211 <= 211."
    This reverts commit 7a74e7032720c128bae68a2898eb72caf8d0dc60.

--- src/Scrutor/AttributeSelector.cs
+++ src/Scrutor/AttributeSelector.cs
@@ -8,12 +8,12 @@ namespace Scrutor;
 
 internal class AttributeSelector : ISelector
 {
-    public AttributeSelector(IEnumerable<System.Type> types)
+    public AttributeSelector(IEnumerable<Type> types)
     {
         Types = types;
     }
 
-    private IEnumerable<System.Type> Types { get; }
+    private IEnumerable<Type> Types { get; }
 
     void ISelector.Populate(IServiceCollection services, RegistrationStrategy? registrationStrategy)
     {
