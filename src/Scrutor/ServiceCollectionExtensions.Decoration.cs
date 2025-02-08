c68def6512663be6b4ca510cbbeb654638d0980d
NoReply Amazon <no-reply@amazon.com>
2025-02-08 03:35:44 +0000

    Revert "[Commit] Iteration 493 #errors = 211 <= 211."
    This reverts commit f8bbf9eaef955a143708db7301e8a174b39157e1.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using System.Collections;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
