7f4e3d3eaee9c300f0d32ce4e6f9250eb50ce786
NoReply Amazon <no-reply@amazon.com>
2025-02-08 03:49:09 +0000

    Revert "[Commit] Iteration 516 #errors = 223 <= 223."
    This reverts commit 39fd951999e660d5290bd64d8532a3dd98a4f03c.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -1,10 +1,8 @@
 using Scrutor;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using Microsoft.Extensions.DependencyInjection;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -96,7 +94,7 @@ public static partial class ServiceCollectionExtensions
         return services.Decorate<TService>((service, _) => decorator(service));
     }
 
-    public static IServiceCollection Decorate<TService>(this IEnumerable<ServiceDescriptor> services, Func<TService, IServiceProvider, TService> decorator) where TService : notnull
+    public static IServiceCollection Decorate<TService>(this System.Collections.Generic.IEnumerable<Microsoft.Extensions.DependencyInjection.ServiceDescriptor> services, Func<TService, System.IServiceProvider, TService> decorator) where TService : notnull
     {
         Preconditions.NotNull(services, nameof(services));
         Preconditions.NotNull(decorator, nameof(decorator));
