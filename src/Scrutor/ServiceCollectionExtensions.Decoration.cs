deb9a147f3245af6e513cc4935a541a443eea87b
NoReply Amazon <no-reply@amazon.com>
2025-02-08 09:13:58 +0000

    [Commit] Iteration 1044 #errors = 211 <= 212.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0452][96,25] The type 'TService' must be a reference type in order to use it as parameter 'TService' in the generic type or method 'ServiceCollectionExtensions.Decorate<TService>(IServiceCollection, Func<TService, IServiceProvider, TService>)'``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -88,7 +88,7 @@ public static partial class ServiceCollectionExtensions
     /// <exception cref="DecorationException">If no service of <typeparamref name="TService"/> has been registered.</exception>
     /// <exception cref="ArgumentNullException">If either the <paramref name="services"/>
     /// or <paramref name="decorator"/> arguments are <c>null</c>.</exception>
-    public static IServiceCollection Decorate<TService>(this IServiceCollection services, Func<TService, TService> decorator) where TService : notnull
+    public static IServiceCollection Decorate<TService>(this IServiceCollection services, Func<TService, TService> decorator) where TService : class
     {
         Preconditions.NotNull(services, nameof(services));
         Preconditions.NotNull(decorator, nameof(decorator));
