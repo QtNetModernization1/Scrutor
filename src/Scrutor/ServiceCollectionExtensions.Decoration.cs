75f8081cb97a86e73703400e127976bbe87771e0
NoReply Amazon <no-reply@amazon.com>
2025-02-08 09:32:32 +0000

    [Commit] Iteration 1073 #errors = 211 <= 211.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Scrutor/ServiceCollectionExtensions.Decoration.cs][CS0012][113,25] The type 'ICollection<>' is defined in an assembly that is not referenced. You must add a reference to assembly 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'.``.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections;
+#if NETSTANDARD2_0
+using System.Runtime.CompilerServices;
+[assembly: TypeForwardedTo(typeof(System.Collections.Generic.ICollection<>))]
+#endif
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
