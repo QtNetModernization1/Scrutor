30b7f128b966da6f7ebfd754e57031a153989f2f
NoReply Amazon <no-reply@amazon.com>
2025-02-08 06:17:39 +0000

    Revert "[Commit] Iteration 756 #errors = 211 <= 211."
    This reverts commit 4ad846d26319051654a5a30bcf498bf35dd06ff4.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -107,8 +106,8 @@ public static partial class ServiceCollectionExtensions
     /// or <paramref name="decorator"/> arguments are <c>null</c>.</exception>
     public static bool TryDecorate<TService>(this IServiceCollection services, Func<TService, TService> decorator) where TService : notnull
     {
-        if (services == null) throw new ArgumentNullException(nameof(services));
-        if (decorator == null) throw new ArgumentNullException(nameof(decorator));
+        Preconditions.NotNull(services, nameof(services));
+        Preconditions.NotNull(decorator, nameof(decorator));
 
         return services.TryDecorate<TService>((service, _) => decorator(service));
     }
