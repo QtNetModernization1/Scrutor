88c8c25172bc985388eb5604c02768f06804f4a6
NoReply Amazon <no-reply@amazon.com>
2025-02-08 09:09:25 +0000

    Revert "[Commit] Iteration 1037 #errors = 212 <= 212."
    This reverts commit 473672860ddda5b8e2abb0ab1c9c3d13462a5160.

--- src/Scrutor/ServiceCollectionExtensions.Decoration.cs
+++ src/Scrutor/ServiceCollectionExtensions.Decoration.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections;
-using System.Runtime.CompilerServices;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -95,12 +94,6 @@ public static partial class ServiceCollectionExtensions
         Preconditions.NotNull(decorator, nameof(decorator));
 
         return services.Decorate<TService>((service, _) => decorator(service));
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static TService DecorateService<TService>(TService service, IServiceProvider _) where TService : notnull
-    {
-        return service;
     }
 
     /// <summary>
